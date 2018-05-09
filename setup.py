558e85fde6ecee5c01731bb0f0a54c4e958756de
Administrator <jean-philippem@riotinto.com>
2018-05-09 09:48:27 +0800

    adding setup.py

---
+++ setup.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python
+
+import os
+import sys
+
+from pkg_resources import Requirement, resource_filename
+
+filename = resource_filename(Requirement.parse("MyProject"),"sample.conf")
+
+from setuptools import setup
+
+version = "0.1.0"
+
+if sys.argv[-1] == 'publish':
+    os.system('python setup.py sdist upload')
+    os.system('python setup.py bdist_wheel upload')
+    sys.exit()
+
+if sys.argv[-1] == 'tag':
+    os.system("git tag -a %s -m 'version %s'" % (version, version))
+    os.system("git push --tags")
+    sys.exit()
+
+with open('README.md') as readme_file:
+    readme = readme_file.read()
+
+with open('HISTORY.md') as history_file:
+    history = history_file.read().replace('.. :changelog:', '')
+
+requirements = [
+]
+
+# http://blog.prabeeshk.com/blog/2014/10/31/install-apache-spark-on-ubuntu-14-dot-04/
+
+long_description = readme + '\n\n' + history
+
+if sys.argv[-1] == 'readme':
+    print(long_description)
+    sys.exit()
+
+
+ENTRY_POINTS = {
+}
+
+setup(
+    name='seismic-processing-platform',
+    version=version,
+    description=('Python library for processing of seismic data'),
+    long_description=long_description,
+    author='seismic-processing-platform development team',
+    author_email='d',
+    url='',
+    packages=[
+        'spp',
+    ],
+    package_dir={'seismic-processing-platform': 'spp'},
+    entry_points=ENTRY_POINTS,
+    include_package_data=True,
+    install_requires=requirements,
+    license='GNU Lesser General Public License, Version 3 (LGPLv3)',
+    zip_safe=False,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: Science/Research',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'License :: OSI Approved :: GNU General Public License v3',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Topic :: Scientific/Engineering',
+    ],
+    keywords=(
+        'microquake, seismology, mining, microseismic, signal processing, '
+        'event location, 3D velocity, automatic, processing, Python, '
+        'focal mechanism'
+    ),
+)
+
+
+
+
+
