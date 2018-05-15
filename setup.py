a60e5ca5a6375f78a014cbfa7334bc1548a01782
Administrator <jean-philippem@riotinto.com>
2018-05-15 13:58:17 +0800

    adding the toolz requirement in setup.py

--- setup.py
+++ setup.py
@@ -25,7 +25,8 @@ with open('HISTORY.md') as history_file:
 
 requirements = [
 		'pathos',
-		'pyyaml'
+		'pyyaml',
+		'toolz'
 ]
 
 # http://blog.prabeeshk.com/blog/2014/10/31/install-apache-spark-on-ubuntu-14-dot-04/
