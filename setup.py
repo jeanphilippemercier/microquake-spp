808cc76d55274f5d6ad436f72606bdb96a54e455
Administrator <jean-philippem@riotinto.com>
2018-05-15 13:33:39 +0800

    adding the pathos package in the requirement in setup.py

--- setup.py
+++ setup.py
@@ -24,6 +24,7 @@ with open('HISTORY.md') as history_file:
     history = history_file.read().replace('.. :changelog:', '')
 
 requirements = [
+pathos
 ]
 
 # http://blog.prabeeshk.com/blog/2014/10/31/install-apache-spark-on-ubuntu-14-dot-04/
