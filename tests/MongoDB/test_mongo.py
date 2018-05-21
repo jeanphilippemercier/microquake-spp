d8f7052f5b680b9f7843f4f47ad5fd2029b58432
jpmercier <jpmercier01@gmail.com>
2018-05-21 14:23:32 +0800

    updated the test functions to match change in microquake
    Former-commit-id: 7748fbc923ac1a7fad0d8f08f8aa9ce6f0e0192d

--- tests/MongoDB/test_mongo.py
+++ tests/MongoDB/test_mongo.py
@@ -1,18 +1,20 @@
 from microquake.core import read_events,stream
 from microquake.db.mongo import mongo
-
+reload(mongo)
 
 # Testing saving and loading Event
 cat = read_events('nll_processed.xml')
+event = cat[10]
 print("connecting to DB")
 db = mongo.connect(uri='mongodb://localhost:27017/', db_name='test')
 print("inserting into DB")
-mongo.insert_event(db, cat[-1], 'h', 'hh', '10', catalog_index=0)
+wf=stream.read("20170419_153133.mseed")
+mongo.insert_event(db, event, catalog_index=0)
 # read event
 print("Reading Event...")
-json_event = mongo.read_event(db,'h','hh')
+json_event = mongo.read_event(db, event.resource_id.id)
 print(json_event)
-decoded_ev = mongo.read_full_event(db,'h','hh')
+decoded_ev = mongo.read_full_event(db, event.resource_id.id)
 print(decoded_ev)
 
 # Testing waveform saving and loading
@@ -20,13 +22,13 @@ print(decoded_ev)
 wf = stream.read("20170419_153133.mseed")
 # json_wf = json.dumps(wf, default=d)
 print("Waveform:")
-id = mongo.insert_waveform(db, wf, 'h', 'hh')
+id = mongo.insert_waveform(db, wf, event.resource_id.id)
 print(id)
 # read waveform
 print("Reading Waveform...")
-json_waveform = mongo.read_waveform(db,'h','hh')
+json_waveform = mongo.read_waveform(db, event.resource_id.id)
 print(json_waveform)
-decoded_wf = mongo.read_full_waveform(db,'h','hh')
+decoded_wf = mongo.read_full_waveform(db, event.resource_id.id)
 print(decoded_wf)
 
 
