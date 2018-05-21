e29b92a3b79c2131bc6239fb74a9b6240bc68522
Hanee Medhat <hanee.medhat@toptal.com>
2018-05-21 02:04:50 +0200

    - Added calls in test_mongo.py to test read and write event and waveforms.
    Former-commit-id: 975fd78f10867380962d0283913437242d6a14f1

--- tests/MongoDB/test_mongo.py
+++ tests/MongoDB/test_mongo.py
@@ -1,8 +1,32 @@
-from microquake.core import read_events
-from microquake.db import mongo
+from microquake.core import read_events,stream
+from microquake.db.mongo import mongo
 
+
+# Testing saving and loading Event
 cat = read_events('nll_processed.xml')
+print("connecting to DB")
 db = mongo.connect(uri='mongodb://localhost:27017/', db_name='test')
+print("inserting into DB")
+mongo.insert_event(db, cat[-1], 'h', 'hh', '10', catalog_index=0)
+# read event
+print("Reading Event...")
+json_event = mongo.read_event(db,'h','hh')
+print(json_event)
+decoded_ev = mongo.read_full_event(db,'h','hh')
+print(decoded_ev)
 
-db = mongo.connect(uri='mongodb://localhost:27017/', db_name='test')
-mongo.insert_event(db, cat[-1], '', '', '', catalog_index=0)
+# Testing waveform saving and loading
+# read mseed waveform
+wf = stream.read("20170419_153133.mseed")
+# json_wf = json.dumps(wf, default=d)
+print("Waveform:")
+id = mongo.insert_waveform(db, wf, 'h', 'hh')
+print(id)
+# read waveform
+print("Reading Waveform...")
+json_waveform = mongo.read_waveform(db,'h','hh')
+print(json_waveform)
+decoded_wf = mongo.read_full_waveform(db,'h','hh')
+print(decoded_wf)
+
+
