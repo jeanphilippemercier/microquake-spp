bb8d46cbaad2f759bc2358bf57d9d62a7d0e4698
Hanee Medhat <hanee.medhat@toptal.com>
2018-05-26 15:22:20 +0200

    finalizing the enhancements and adding modification_time in DB objects
    Former-commit-id: 56bc1d90c1547be51263f3f5acf9552371499b59

--- tests/MongoDB/test_mongo.py
+++ tests/MongoDB/test_mongo.py
@@ -1,5 +1,5 @@
 from microquake.core import read_events,stream
-from microquake.db.mongo.mongo import MongoHandler
+from microquake.db.mongo.mongo import MongoDBHandler, EventDB, StreamDB
 import yaml
 import os
 # reload(mongo)
@@ -16,16 +16,16 @@ with open(config_file,'r') as cfg_file:
 # Testing saving and loading Event
 cat = read_events(config_dir + "../data/" + 'nll_processed.xml')
 event = cat[10]
-
 print("connecting to DB")
-db = MongoHandler(uri=params['uri'], db_name=params['db_name'])
+db = MongoDBHandler(uri=params['uri'], db_name=params['db_name'])
 print("inserting into DB")
-db.insert_event(event, catalog_index=0)
+event_db = EventDB(db)
+event_db.insert_event(event, catalog_index=0)
 # read event
 print("Reading Event...")
-json_event = db.read_event(event.resource_id.id)
+json_event = event_db.read_event(event.resource_id.id)
 print(json_event)
-decoded_ev = db.read_full_event(event.resource_id.id)
+decoded_ev = event_db.read_full_event(event.resource_id.id)
 print(decoded_ev)
 
 # Testing waveform saving and loading
@@ -33,13 +33,16 @@ print(decoded_ev)
 wf = stream.read(config_dir + "../data/" + "20170419_153133.mseed")
 # json_wf = json.dumps(wf, default=d)
 print("Waveform:")
-id = db.insert_waveform(wf, event.resource_id.id)
+stream_db = StreamDB(db)
+id = stream_db.insert_waveform(wf, event.resource_id.id)
 print(id)
 # read waveform
 print("Reading Waveform...")
-json_waveform = db.read_waveform(event.resource_id.id)
+json_waveform = stream_db.read_waveform(event.resource_id.id)
 print(json_waveform)
-decoded_wf = db.read_full_waveform(event.resource_id.id)
+decoded_wf = stream_db.read_full_waveform(event.resource_id.id)
 print(decoded_wf)
+print("Closing Connection...")
+db.disconnect()
 
 
