991dd0120fa144ab5d678d3dea6f83167a141404
Hanee Medhat <hanee.medhat@toptal.com>
2018-06-02 15:15:38 +0200

    - Implemented Kafka integration and sending MSEED to kafka cluster. - Also create new test kafka consumer to read the MSEED as Streams
    Former-commit-id: 6ceb6cf05f2ca79e8fd819dfc77802be32db3b36

--- scripts/ims_connector.py
+++ scripts/ims_connector.py
@@ -1,7 +1,18 @@
 from spp.ims_connector import core
 from importlib import reload
+from scripts.kafka_utils import KafkaHandler
+from io import BytesIO
 
 reload(core)
+
+
+def write_to_kafka(kafka_handler_obj, kafka_topic, stream_object):
+    buf = BytesIO()
+    stream_object.write(buf, format='MSEED')
+    encoded_obj = serializer.encode_base64(buf)
+    msg_key = str(stream_object[0].stats.starttime)
+    print("sending to kafka...", "key:", msg_key, "msg size:", sys.getsizeof(encoded_obj) / 1024 / 1024, "MB")
+    kafka_handler_obj.send_to_kafka(kafka_topic, serializer.encode_base64(buf), msg_key.encode('utf-8'))
 
 
 if __name__ == "__main__":
@@ -11,6 +22,7 @@ if __name__ == "__main__":
     import os
     import yaml
     from microquake.core.event import Catalog
+    from microquake.core.util import serializer
     import numpy as np
 
     config_dir = os.environ['SPP_CONFIG']
@@ -22,19 +34,33 @@ if __name__ == "__main__":
         params = yaml.load(cfg_file)
         params = params['ims_connector']
 
+
+    # Create Kafka Object
+    kafka = KafkaHandler(params['kafka']['brokers'])
+    kafka_topic = params['kafka']['topic']
+
     if params['data_source']['type'] == 'remote':
         for st in core.request_handler():
             print(st)
-
             # write to Kafka
+            write_to_kafka(kafka, kafka_topic, st)
 
     elif params['data_source']['type'] == 'local':
         location = params['data_source']['location']
         period = params['period']
         window_length = params['window_length']
 
+        import sys
         for i in np.arange(0, period, window_length):
             st = core.request_handler_local(location)
-            print(st)
+            print("---------------------------> ", type(st))
+            # print(st)
+            #  write to Kafka
+            print("(", i, " from", period, ")")
+            write_to_kafka(kafka,kafka_topic, st)
 
-        # write to Kafka
+    print("Flushing and Closing Kafka....")
+    kafka.producer.flush()
+    print("Program Exit")
+
+
---
+++ scripts/kafka_utils.py
@@ -0,0 +1,19 @@
+from kafka import KafkaProducer, KafkaConsumer
+
+
+class KafkaHandler:
+
+    def __init__(self, brokers_list):
+        self.producer = KafkaProducer(bootstrap_servers=brokers_list)
+
+    def send_to_kafka(self, topic_name, message, key=None):
+        if key is None:
+            self.producer.send(topic=topic_name, value=message)
+        else:
+            self.producer.send(topic=topic_name, key=key, value=message)
+
+    @staticmethod
+    def consume_from_topic(topic_name, brokers_list):
+        return KafkaConsumer(topic_name,
+                                 #group_id='my-group',
+                                 bootstrap_servers=brokers_list)
