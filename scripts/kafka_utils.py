b1e9549b190ec56ec4b10cb16959bdee422a4ae9
Hanee Medhat <hanee.medhat@toptal.com>
2018-06-04 23:15:35 +0200

    - modify kafka producer max message size to fit > 15 MB
    Former-commit-id: a26b403082f733b9d713cd3ba06863d0ccc7b61a

--- scripts/kafka_utils.py
+++ scripts/kafka_utils.py
@@ -4,7 +4,7 @@ from kafka import KafkaProducer, KafkaConsumer
 class KafkaHandler:
 
     def __init__(self, brokers_list):
-        self.producer = KafkaProducer(bootstrap_servers=brokers_list)
+        self.producer = KafkaProducer(bootstrap_servers=brokers_list, max_request_size=20971520)
 
     def send_to_kafka(self, topic_name, message, key=None):
         if key is None:
