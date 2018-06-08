63c6ff72df28bec736899b5f0bebb5553b73a881
jpmercier <jpmercier01@gmail.com>
2018-06-08 11:37:50 +0800

    created structure for the creating and reading avro objects, the schema is in config/mseed_avro_schema.avsc
    Former-commit-id: dcaea2b90f979a6b1045bde90d7639f146f7c5ab

--- spp/utils/core.py
+++ spp/utils/core.py
@@ -5,3 +5,31 @@ def get_stations():
     station_file = os.path.join(common, 'sensors.csv')
 
     return read_stations(station_file, format='CSV', has_header=True)
+
+
+def encode_avro(stream):
+    """
+    This function needs to be completed
+    :param stream:
+    :return:
+    """
+    import os
+
+    config_dir = os.environ['SPP_CONFIG']
+
+    avro = None
+
+    return avro
+
+
+def decode_avro(avro):
+    """
+    this function needs to be completed
+    :param avro:
+    :return:
+    """
+
+    stream = none
+
+    return stream
+
