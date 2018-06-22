07412adee88ff9ebe68b0064d564972ee5df71d9
Hanee Medhat <hanee.medhat@toptal.com>
2018-06-22 23:59:07 +0200

    Enhancing some logging for better analysis
    Former-commit-id: e0abdb6ebf62b05cfadbb6238580fbcea96a7d10

--- scripts/ims_connector.py
+++ scripts/ims_connector.py
@@ -12,16 +12,20 @@ def write_to_kafka(kafka_handler_obj, kafka_topic, stream_object):
     s_time = time.time()
     buf = BytesIO()
     stream_object.write(buf, format='MSEED')
-    print("==> Normal:", sys.getsizeof(buf) / 1024 / 1024)
     kafka_msg = buf.getvalue() #serializer.encode_base64(buf)
     msg_key = str(stream_object[0].stats.starttime)
-    e_time = time.time() - s_time
-    print("==> object preparation took:", e_time)
-    print("==> sending to kafka...", "key:", msg_key, "msg size:", sys.getsizeof(kafka_msg) / 1024 / 1024, "MB")
+    end_time_preparation = time.time() - s_time
+
+    msg_size = (sys.getsizeof(kafka_msg) / 1024 / 1024)
+
     s_time = time.time()
     kafka_handler_obj.send_to_kafka(kafka_topic, kafka_msg, msg_key.encode('utf-8'))
-    e_time = time.time() - s_time
-    print("==> object submission took:", e_time)
+    end_time_submission = time.time() - s_time
+
+    print("==> Object Size:", "%.2f" % msg_size, "MB",
+          "Key:", msg_key,
+          ", Preparation took:", "%.2f" % end_time_preparation,
+          ", Submission took:", "%.2f" % end_time_submission)
 
 if __name__ == "__main__":
 
@@ -61,23 +65,22 @@ if __name__ == "__main__":
         stime = time.time()
 
         for i in np.arange(0, period, window_length):
-            print("==> (", i, " from", period, ")")
+            print("==> Processing (", i, " from", period, ")")
             s_time = time.time()
             st = core.request_handler_local(location)
             e_time = time.time() - s_time
-            print("==> Fetching File took: ", e_time)
+            print("==> Fetching File took: ", "%.2f" % e_time)
             #  write to Kafka
             write_to_kafka(kafka,kafka_topic, st)
-
 
         print("==> Flushing and Closing Kafka....")
         s_time = time.time()
         kafka.producer.flush()
         e_time = time.time() - s_time
-        print("==> Flushing Kafka took: ", e_time)
+        print("==> Flushing Kafka took: ", "%.2f" % e_time)
 
-        etime = time.time()
-        print("==> Total Time Taken: ", etime - stime)
+        etime = time.time() - stime()
+        print("==> Total Time Taken: ", "%.2f" % etime)
 
         print("==> Program Exit")
 
