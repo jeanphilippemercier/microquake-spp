100f62551d9c8ca1f7f83fae5246befc8f2a7c8c
jpmercier <jpmercier01@gmail.com>
2018-11-03 19:24:02 +0800

    uploading grid server
    Former-commit-id: 5e06eb09c66cabde9b057db2009b614a4d04d78a

--- servers/grid_servers.py
+++ servers/grid_servers.py
@@ -1,71 +1,83 @@
 #!flask/bin/python
-from flask import Flask, jsonify, send_file
-from io import BytesIO
-#
-from spp.travel_time import get_travel_time_grid
+from flask import Flask, send_file, jsonify
+from flask_restful import Resource, Api, reqparse
 from spp.utils import get_stations
-import pickle
-from base64 import b64encode
 import numpy as np
-from spp.travel_time import get_travel_time_grid_raw
+from spp.travel_time import get_travel_time_grid_raw, get_travel_time_grid
+from io import BytesIO
+from time import time
+from flask_restful.inputs import boolean
 
 app = Flask(__name__)
+api = Api(app)
 
-#grids = [get_travel_time_grid(station.code, 'P') for station in
 site = get_stations()
 
-
 grids = []
+mq_grids = []
+stations = []
+phases = []
 
-for station in site.stations()[:10]:
+start = time()
+for station in site.stations():
     for phase in ['P', 'S']:
-        print(station.code, phase)
-        bytes_io = get_travel_time_grid_raw(station.code, phase)
-        print(bytes_io)
-        grids.append(bytes_io)
+        data = get_travel_time_grid_raw(station.code, phase)
+        mq_grid = get_travel_time_grid(station.code, phase)
+        stations.append(int(station.code))
+        phases.append(phase)
+        grids.append(data)
+        mq_grids.append(mq_grid)
+end = time()
+print(end - start)
 
-    # grids.append(get_travel_time_grid(station.code, 'P').__dict__)
-    # grids.append(get_travel_time_grid(station.code, 'S').__dict__)
-
-
-
-    # print(station.code)
-    # grids['grids'].append(get_travel_time_grid(station.code, 'P'))
-    # grids['grids'].append(get_travel_time_grid(station.code, 'S'))
-
+stations = np.array(stations)
+phases = np.array(phases)
+grids = np.array(grids)
 
 
-# @app.route('/')
-# def index():
-#     return "Hello, World!"
+class TravelTimeGrid(Resource):
+    def get(self):
+        args = parser.parse_args()
+        st_id = args['st_id']
+        phase = args['phase']
+        index = np.nonzero((stations == st_id) & (phases == phase))[0]
+        print(index)
+        return send_file(BytesIO(grids[index][0]), as_attachment=True,
+                         attachment_filename='test.bin',
+                         mimetype='application/octet-stream')
 
+class GridHeader(Resource):
+    pass
+    # return jsonify(grid_header)
 
-# tasks = [
-#     {
-#         'id': 1,
-#         'title': u'Buy groceries',
-#         'description': u'Milk, Cheese, Pizza, Fruit, Tylenol',
-#         'done': o
-#     },
-#     {
-#         'id': 2,
-#         'title': u'Learn Python',
-#         'description': u'Need to find a good Python tutorial on the web',
-#         'done': False
-#     }
-# ]
+class TravelTimePoint(Resource):
+    def get(self):
+        args = parser.parse_args()
+        st_id = args['st_id']
+        phase = args['phase']
+        x = args['x']
+        y = args['y']
+        z = args['z']
+        grid_coordinates = args['grid_coordinates']
+        print(st_id, phase, grid_coordinates)
+        index = np.nonzero((stations == st_id) & (phases == phase))[0][0]
+        print(index)
+        mq_grid = mq_grids[index]
+        tt = mq_grid.interpolate((x, y, z), grid_coordinate=grid_coordinates)
+        print(tt[0])
+        return jsonify({'value': tt[0]})
 
-@app.route('/todo/api/v1.0/grid/<int:st_id>', methods=['GET'])
-def get_tasks(st_id):
-    # return grids['grids'][0]
-    # data_connect_params = get_data_connector_parameters()
-    # return jsonify({'data_connector': grids[st_id]})
-    # obj = BytesIO()
-    # pickle.dump(grids[st_id], obj)
-    # obj.seek(0)
-    return send_file(grids[st_id], as_attachment=True,
-                     attachment_filename='test.bin',
-                     mimetype='application/octet-stream')
+parser = reqparse.RequestParser()
+parser.add_argument('st_id', type=int)
+parser.add_argument('phase', type=str)
+parser.add_argument('x', type=float)
+parser.add_argument('y', type=float)
+parser.add_argument('z', type=float)
+parser.add_argument('grid_coordinates', type=boolean)
+api.add_resource(TravelTimeGrid, '/travel_time/grid')
+api.add_resource(TravelTimePoint, '/travel_time/time')
+import resource
+print(resource.getrusage(resource.RUSAGE_SELF))
 
 if __name__ == '__main__':
     # grids = get_data()
@@ -73,4 +85,4 @@ if __name__ == '__main__':
     from spp.utils import get_data_connector_parameters, get_stations
     # from spp.travel_time import get_travel_time_grid
     from microquake.core.data.grid import read_grid
-    app.run(debug=False)
+    app.run(debug=True)
