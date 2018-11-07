463fc601b692c8a8452a64bf214c4d001b01846c
jpmercier <jpmercier01@gmail.com>
2018-11-07 18:45:22 +0800

    test code for the mseed_decomposer and recomposer

---
+++ sandbox/JP/mseed_decomposer_test.py
@@ -0,0 +1,49 @@
+# this script decompose the message and send the decomposed mseed to a Kafka
+# queue
+
+from microquake.io.waveform import core
+from importlib import reload
+from microquake.core import Trace, Stream
+import numpy as np
+from pandas import DataFrame
+from spp.utils.kafka import KafkaHandler
+from logging import getLogger
+
+kafka_brokers = ['localhost:9092']
+kafka_topic = 'test'
+
+kafka_handler = KafkaHandler(kafka_brokers)
+
+logger = getLogger(__name__)
+
+reload(core)
+
+trs = []
+for i in np.arange(10):
+    tr = Trace(data=np.random.randn(1000))
+    tr.stats.network = 'TEST'
+    tr.stats.station = '%d' % i
+    tr.stats.channel = 'X'
+    trs.append(tr)
+
+st = Stream(traces=trs)
+
+result = core.mseed_decomposer(st)
+
+df = DataFrame(result)
+
+# needs to be organized by key for the recomposer to work
+df_grouped = df.groupby(['key'])
+
+logger.debug("Grouped DF Stats:" + str(df_grouped.size()))
+
+chunks = []
+for name, group in df_grouped:
+    data = b''
+    for g in group['blob'].values:
+        data += g
+    timestamp = int(name.timestamp() * 1e3)
+    key = name.strftime('%Y-%d-%m %H:%M:%S.%f').encode('utf-8')
+    kafka_handler.send_to_kafka(kafka_topic, message=data, key=key,
+                                timestamp=int(timestamp))
+kafka_handler.producer.flush()
---
+++ sandbox/JP/mseed_recomposer_test.py
@@ -0,0 +1,16 @@
+# This script test the recomposition of mseed from the messages consumed
+from spp.utils.kafka import KafkaHandler
+from microquake.core import read
+from io import BytesIO
+
+kafka_brokers = ['localhost:9092']
+kafka_topic = 'test'
+
+kafka_handle = KafkaHandler(kafka_brokers)
+
+consumer = KafkaHandler.consume_from_topic(kafka_topic, kafka_brokers)
+
+print("Awaiting Kafka mseed messsages")
+for msg_in in consumer:
+    print("Received Key:", msg_in.key)
+    st = read(BytesIO(msg_in.value))
