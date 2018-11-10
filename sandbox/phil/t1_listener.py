1e119d730213bf780b8713d5ab4eae33c485c00b
Philippe Dales <pdales@gmail.com>
2018-11-10 12:59:00 +0800

    nc

--- sandbox/phil/t1_listener.py
+++ sandbox/phil/t1_listener.py
@@ -7,26 +7,25 @@ from importlib import reload
 import numpy as np
 import os
 from io import BytesIO
-# import struct
-# import glob
-# import datetime
-from obspy import read
+from microquake.core import read
 from spp.utils.kafka import KafkaHandler
 import yaml
 
 from xseis2 import xutil
 from xseis2 import xflow
 from xseis2 import xspy
+from microquake.io import msgpack
+
 
 nthreads = int(4)
 debug = int(0)
 dsr = float(3000.)
 wlen_sec = float(1.0)
 
-# common_dir = os.environ['SPP_COMMON']
-# tts_dir = os.path.join(common_dir, 'NLL/time/')
+common_dir = os.environ['SPP_COMMON']
+tts_dir = os.path.join(common_dir, 'NLL/time/')
 # tts_dir = '/mnt/seismic_shared_storage/time/'
-tts_dir = '/home/phil/data/oyu/NLLOC_grids/'
+# tts_dir = '/home/phil/data/oyu/NLLOC_grids/'
 
 
 ttable, stalocs, namedict, gdef = xutil.ttable_from_nll_grids(tts_dir, key="OT.P")
@@ -35,6 +34,7 @@ ttable = (ttable * dsr).astype(np.uint16)
 ngrid = ttable.shape[1]
 tt_ptrs = np.array([row.__array_interface__['data'][0] for row in ttable])
 
+
 config_dir = os.environ['SPP_CONFIG']
 fname = os.path.join(config_dir, 'data_connector_config.yaml')
 
@@ -42,23 +42,23 @@ with open(fname, 'r') as cfg_file:
 	params = yaml.load(cfg_file)
 
 logdir = params['data_connector']['logging']['log_directory']
-# Create Kafka Object
 brokers = params['data_connector']['kafka']['brokers']
-# topic_in = params['data_connector']['kafka']['topic']
-# consumer = KafkaHandler.consume_from_topic(topic_in, brokers)
 
 # topic_in = 'mseed-blocks'
 topic_in = 'mseed-1sec'
-consumer = KafkaHandler.consume_from_topic(topic_in, brokers, group_id='interloc_group')
-
+consumer = KafkaHandler.consume_from_topic(topic_in, brokers, group_id='group')
 topic_out = 'interloc'  # need to get this from a config file later
-
 
 print("Awaiting Kafka mseed messsages")
 for msg_in in consumer:
 	print("Received Key:", msg_in.key)
-	st = read(BytesIO(msg_in.value))
+
+	# st = msgpack.unpack(BytesIO(msg_in.value()))
+	st = msgpack.unpack(msg_in.value)
+	# st = read(BytesIO(msg_in.value))
 	# st = read(mseed_file)
+	# print(st)
+
 	print(st)
 	xflow.prep_stream(st)
 	data, t0, stations, chanmap = xflow.build_input_data(st, wlen_sec, dsr)
@@ -72,10 +72,10 @@ for msg_in in consumer:
 	print("power: %.3f, ix_grid: %d, ix_ot: %d" % (vmax, imax, iot))
 	print("utm_loc: ", lmax.astype(int))
 
-	print("Sending Kafka interloc messsage")
-	msg_out, key_out = xflow.encode_for_kafka(ot_epoch, lmax, vmax)
-	kafka_handler_obj = KafkaHandler(brokers)
-	kafka_handler_obj.send_to_kafka(topic_out, msg_out, key_out)
-	kafka_handler_obj.producer.flush()
-	print("==================================================================")
+	# print("Sending Kafka interloc messsage")
+	# msg_out, key_out = xflow.encode_for_kafka(ot_epoch, lmax, vmax)
+	# kafka_handler_obj = KafkaHandler(brokers)
+	# kafka_handler_obj.send_to_kafka(topic_out, msg_out, key_out)
+	# kafka_handler_obj.producer.flush()
+	# print("==================================================================")
 
