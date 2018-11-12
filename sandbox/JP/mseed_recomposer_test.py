cf1040c00d773bb59d6b63d671f3b788171bc29e
Hanee Medhat <hanee.medhat@toptal.com>
2018-11-12 23:14:06 +0200

    adding integration with new events API for module: 05_event_database_handler

--- sandbox/JP/mseed_recomposer_test.py
+++ sandbox/JP/mseed_recomposer_test.py
@@ -3,8 +3,8 @@ from spp.utils.kafka import KafkaHandler
 from microquake.core import read
 from io import BytesIO
 
-kafka_brokers = ['localhost:9092']
-kafka_topic = 'data_ingestion'
+kafka_brokers = "kafka-node-001:9092,kafka-node-002:9092,kafka-node-003:9092"#['localhost:9092']
+kafka_topic = "mseed_samples_6" #'data_ingestion'
 
 kafka_handle = KafkaHandler(kafka_brokers)
 
@@ -14,3 +14,5 @@ print("Awaiting Kafka mseed messsages")
 for msg_in in consumer:
     print("Received Key:", msg_in.key)
     st = read(BytesIO(msg_in.value))
+    print(st)
+
