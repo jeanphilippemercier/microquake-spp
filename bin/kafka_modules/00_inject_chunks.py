e68452d524739ed1938c654c136d166c181a9616
Philippe Dales <pdales@gmail.com>
2018-11-14 09:20:41 +0800

    added and renamed kafka scripts;

---
+++ bin/kafka_modules/00_inject_chunks.py
@@ -0,0 +1,30 @@
+from importlib import reload
+# import numpy as np
+# import os
+# import glob
+from microquake.core import read
+from spp.utils.kafka import KafkaHandler
+# from io import BytesIO
+# from microquake.io import msgpack
+from spp.utils.application import Application
+# from struct import unpack
+# from datetime import datetime
+# from microquake.core.util import tools
+from microquake.io import waveform
+
+app = Application()
+brokers = app.settings.kafka.brokers
+kaf_handle = KafkaHandler(brokers)
+
+params = app.settings.chunk_injector
+topic_out = params.kafka_producer_topic
+
+st = read(params.path)
+dchunks = waveform.decompose_mseed(st.write_bytes())
+
+print("Sending kafka mseed messsages")
+for tstamp, dat in dchunks.items():
+        key_out = str("keyx").encode('utf-8')
+        # kaf_handle.send_to_kafka(topic_out, key_out, dat, timestamp_ms=tstamp)
+        # kaf_handle.producer.flush()
+        print(key_out, tstamp)
--- sandbox/phil/run_data_transformer.sh
+++ sandbox/phil/run_data_transformer.sh
@@ -1,6 +1,4 @@
 
-
-
 env destination="mseed-blocks" filename=$SPP_COMMON/synthetic/simdat10s.mseed brokers="localhost" java -jar $SPP_COMMON/jars/mseed-loader-1.0-SNAPSHOT.jar
 
 env destination="mseed_1sec" source="mseed-blocks" brokers="localhost" java -jar $SPP_COMMON/jars/mseed-streams-1.0-SNAPSHOT.jar
