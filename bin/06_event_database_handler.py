d09efbad8b7986dae9fd8a906276876436549f35
Hanee Medhat <hanee.medhat@toptal.com>
2018-11-16 22:51:50 +0200

    fix issue #2 by making the DB handler send files of event separated based in its type

--- bin/06_event_database_handler.py
+++ bin/06_event_database_handler.py
@@ -8,20 +8,34 @@ from time import time
 import requests
 
 
-def construct_file(global_filename, file_bytes):
-    file_bytes.name = global_filename
+def construct_file(file_bytes, global_filename=None):
+    if global_filename is not None:
+        file_bytes.name = global_filename
     file_bytes.seek(0)
     return file_bytes
 
 
-def construct_event_files(global_filename,
-                          event_file_bytes,
-                          waveform_file_bytes,
-                          context_file_bytes):
+def construct_all_event_files(event_file_bytes,
+                              waveform_file_bytes,
+                              context_file_bytes, global_filename):
     data = dict()
-    data['event'] = construct_file(global_filename + ".xml", event_file_bytes)
-    data['waveform'] = construct_file(global_filename + ".mseed", waveform_file_bytes)
-    data['context'] = construct_file(global_filename + ".mseed", context_file_bytes)
+    data['event'] = construct_file(event_file_bytes, global_filename + ".xml")
+    data['waveform'] = construct_file(waveform_file_bytes, global_filename + ".mseed")
+    data['context'] = construct_file(context_file_bytes, global_filename + ".mseed")
+    return data
+
+
+def construct_one_event_file(event_id, file_type, file_bytes, global_filename):
+    data = dict()
+
+    data['event_resource_id'] = event_id
+
+    if file_type == 'QUAKEML':
+        data['event'] = construct_file(file_bytes, global_filename + ".xml")
+    elif file_type == 'MSEED':
+        data['waveform'] = construct_file(file_bytes, global_filename + ".mseed")
+    elif file_type == 'CONTEXT_MSEED':
+        data['context'] = construct_file(file_bytes, global_filename + ".mseed")
     return data
 
 
@@ -56,7 +70,9 @@ if __name__ == "__main__":
         logger.info('unpacking the data received from Kafka topic <%s>'
                     % settings.magnitude.kafka_consumer_topic)
         t1 = time()
+
         data = msgpack.unpack(message.value)
+        # event_id, file_type, data = msgpack.unpack(message.value)
 
         # These might not be used as we will send the files bytes direct
         # st = read(BytesIO(data[1]))
@@ -65,15 +81,14 @@ if __name__ == "__main__":
         # We need to extract filename from event file as example:
         global_filename = "test"
         # Sending the context same as stream file, just for now as temp solution
-        files_dict = construct_event_files(global_filename, BytesIO(data[1]),
-                                           BytesIO(data[2]), BytesIO(data[2]))
+        files_dict = construct_one_event_file(BytesIO(data[0]), BytesIO(data[1]),
+                                              BytesIO(data[2]), global_filename)
         post_event_files_to_api(EVENTS_API_URL, files_dict)
 
         t2 = time()
         logger.info('done unpacking the data from Kafka topic <%s> in '
                     '%0.3f seconds'
                     % (settings.magnitude.kafka_consumer_topic, t2 - t1))
-
 
         # JP: HANEE YOU SHOULD BE ABLE TO INSERT GET IT FROM THERE
         # ALTERNATIVELY YOU COULD COMMENT THE LINE ABOVE AND SIMPLE READ THE
@@ -84,33 +99,3 @@ if __name__ == "__main__":
         # cat = read_events('test.xml')
         # cat = Catalog(events=[cat[0]])
         # st = read('2018-11-08T10:21:49.898496Z.mseed')
-
-        # IF YOU MODIFY THE CONFIG.TOML FILE PLEASE MAKE SURE YOU UPDATE THE
-        # CONFIG.TOML.EXAMPLE FILE
-
-
-
-
-        # OLD CODE BELOW
-
-        # should use the REST API POST function instead of the DB handler
-        # logger.info('received a message')
-        # stime = time.time()
-        # stream = read(BytesIO(message.value))
-        # etime = time.time() - stime
-        #
-        # msg_size = (sys.getsizeof(stream) / 1024 / 1024)
-        # logger.info("==> consumed stream object from kafka in:", "%.2f" % etime,
-        #       "Stream Size:", "%.2f" % msg_size, "MB")
-        #
-        # stime = time.time()
-        # traces_list = stream.to_traces_json()
-        # etime = time.time() - stime
-        # logger.info("==> converted stream object to json in:", "%.2f" % etime)
-        #
-        # stime = time.time()
-        # mongo_conn.insert_many(collection_name, traces_list)
-        # etime = time.time() - stime
-        # logger.info("==> inserted stream data into MongoDB in:", "%.2f" % etime)
-        #
-        # logger.info("=========================================================")
