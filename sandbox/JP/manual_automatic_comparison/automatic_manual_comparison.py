65aa73fbfad5f3c1df89ef74697e6905a957723d
jpmercier <jpmercier01@gmail.com>
2018-12-10 10:12:37 +0800

    nc

---
+++ sandbox/JP/manual_automatic_comparison/automatic_manual_comparison.py
@@ -0,0 +1,39 @@
+from spp.utils.seismic_client import get_events_catalog
+from microquake.core import UTCDateTime
+from spp.utils.application import Application
+import matplotlib.pyplot as plt
+import numpy as np
+
+app = Application()
+
+api_base_url = app.settings.seismic_api.base_url
+site = app.get_stations()
+
+start_time = UTCDateTime(2018, 11, 20)
+end_time =UTCDateTime(2018, 12, 1)
+
+request_events = get_events_catalog(api_base_url, start_time, end_time)
+
+for request_event in request_events:
+    st = request_event.get_waveform()
+    cat = request_event.get_event()
+    print(cat[0].preferred_origin())
+
+    ot = cat[0].preferred_origin().time
+
+    dist = np.linalg.norm(cat[0].preferred_origin().loc - cat[0].origins[0].loc)
+    print(dist)
+
+    plt.figure(1)
+    plt.clf()
+    st.distance_time_plot(cat[0], site)
+    plt.title('automatic-distance: %d' % dist)
+    plt.savefig('plots/' + str(ot) + '_automatic.pdf')
+
+    cat[0].preferred_origin_id = cat[0].origins[0].resource_id.id
+
+    plt.figure(2)
+    plt.clf()
+    st.distance_time_plot(cat[0], site)
+    plt.title('manually-distance: %d' % dist)
+    plt.savefig('plots/' + str(ot) + '_manual.pdf')
--- spp/utils/seismic_client.py
+++ spp/utils/seismic_client.py
@@ -3,6 +3,7 @@ from microquake.core.stream import *
 from io import BytesIO
 import requests
 from dateutil import parser
+from microquake.core import UTCDateTime
 
 
 class RequestEvent:
@@ -11,7 +12,7 @@ class RequestEvent:
             if 'time' in key:
                 if type(ev_dict[key]) is not str:
                     continue
-                setattr(self, key, parser.parse(ev_dict[key]))
+                setattr(self, key, UTCDateTime(parser.parse(ev_dict[key])))
             else:
                 setattr(self, key, ev_dict[key])
 
@@ -39,107 +40,166 @@ class RequestEvent:
         return self.__dict__.keys()
 
 
-def build_request_data_from_files(event_id, event_file, mseed_file,
-                                  mseed_context_file):
-    files = dict()
-
-    # prepare event
-    if event_file is not None:
-        evt = read_events(event_file)
-
-        if event_id is None:
-            event_id = evt[0].resource_id.id
-
-        evt_bytes = BytesIO()
-        evt.write(evt_bytes, format="QUAKEML")
-        evt_bytes.name = event_file
-        evt_bytes.seek(0)
-        files['event'] = evt_bytes
-
-    # prepare waveform
-    if mseed_file is not None:
-        wf = read(mseed_file, format='MSEED')
-        wf_bytes = BytesIO()
-        wf.write(wf_bytes)
-        wf_bytes.name = mseed_file
-        wf_bytes.seek(0)
-        files['waveform'] = wf_bytes
-
-    # prepare context waveform
-    if mseed_context_file is not None:
-        wfc = read(mseed_context_file, format='MSEED')
-        wfc_bytes = BytesIO()
-        wfc.write(wfc_bytes)
-        wfc_bytes.name = mseed_context_file
-        wfc_bytes.seek(0)
-        files['context'] = wfc_bytes
-
-    data = dict()
-    data['event_resource_id'] = str(event_id)
-
-    print(data, files)
-
-    return data, files
-
-
-def build_request_data_from_object(event_id=None, event=None, stream=None,
-                                   context_stream=None):
+def post_data_from_files(api_base_url, event_id=None, event_file=None,
+                         mseed_file=None, mseed_context_file=None,
+                         tolerance=0.5, logger=None):
     """
-    Build request directly from object
+    Build request directly from objects
+    :param api_base_url: base url of the API
     :param event_id: event_id (not required if an event is provided)
-    :param event: microquake.core.Event.event
-    :param stream: event seismogram (microquake.core.Stream.stream)
-    :param context_stream: context seismogram trace (
-    microquake.core.Stream.stream)
+    :param event_file: path to a QuakeML file
+    :param stream_file: path to a mseed seismogram file
+    :param context_stream_file: path to a context seismogram file
+    :param tolerance: Minimum time between an event already in the database
+    and this event for this event to be inserted into the database. This is to
+    avoid duplicates. event with a different
+    <event_id> within the <tolerance> seconds of the current object will
+    not be inserted. To disable this check set <tolerance> to None.
+    :param logger: a logging.Logger object
     :return: same as build_request_data_from_bytes
     """
 
-    event_id=None
+    __name__ = 'spp.utils.post_data_from_files'
+
+    if logger is None:
+        import logging
+        logger = logging.getLogger(__name__)
+
+    event=None,
+    stream=None
+    context_stream=None
+
+    # read event
+    if event_file is not None:
+        event = read_events(event_file)
+
+    # read waveform
+    if mseed_file is not None:
+        stream = read(mseed_file, format='MSEED')
+
+    # read context waveform
+    if mseed_context_file is not None:
+        context_stream = read(mseed_context_file, format='MSEED')
+
+    return post_data_from_objects(api_base_url, event_id=event_id,
+                                  event=event, stream=stream,
+                                  context_stream=context_stream,
+                                  tolerance=tolerance, logger=logger)
+
+
+def post_data_from_objects(api_base_url, event_id=None, event=None,
+                          stream=None, context_stream=None, tolerance=0.5,
+                          logger=None):
+    """
+    Build request directly from objects
+    :param api_base_url: base url of the API
+    :param event_id: event_id (not required if an event is provided)
+    :param event: microquake.core.event.Event or a
+    microquake.core.event.Catalog containing a single event. If the catalog
+    contains more than one event, only the first event, <catalog[0]> will be
+    considered. Use catalog with caution.
+    :param stream: event seismogram (microquake.core.Stream.stream)
+    :param context_stream: context seismogram trace (
+    microquake.core.Stream.stream)
+    :param tolerance: Minimum time between an event already in the database
+    and this event for this event to be inserted into the database. This is to
+    avoid duplicates. event with a different
+    <event_id> within the <tolerance> seconds of the current object will
+    not be inserted. To disable this check set <tolerance> to None.
+    :param logger: a logging.Logger object
+    :return: same as build_request_data_from_bytes
+    """
+
+    from microquake.core.event import Catalog
+
+    __name__ = 'spp.utils.post_data_from_objects'
+
+    if logger is None:
+        import logging
+        logger = logging.getLogger(__name__)
+
     event_bytes=None
     mseed_bytes=None
     mseed_context_bytes=None
 
+    if type(event) is type(Catalog):
+        event = event[0]
+        logger.warning('a <microquake.core.event.Catalog> object was '
+                       'provided, only the first element of the catalog will '
+                       'be used, this may lead to an unwanted behavior')
+
+    # check if insertion is possible
+    data = {}
     if event is not None:
+        event_time = event.preferred_origin().time
+        event_resource_id = str(event.resource_id)
+    else:
+        if event_id is None:
+            logger.warning('A valid event_id must be provided when no '
+                           '<event> is not provided.')
+            logger.info('exiting')
+            return
+
+        re = get_event_by_id(api_base_url, event_id)
+        if re is None:
+            logger.warning('request did not return any event with the '
+                           'specified event_id. A valid event_id or an event '
+                           'object must be provided to insert a stream or a '
+                           'context stream into the database.')
+            logger.info('exiting')
+            return
+
+        event_time = re.time_utc
+        event_resource_id = str(re.event_resource_id)
+
+    data['event_resource_id'] = event_resource_id
+
+    logger.info('processing event with resource_id: %s' % event_resource_id)
+
+    base_event_file_name = str(event_time)
+    files = {}
+    # Test if there is an event within <tolerance> seconds in the database
+    # with a different <event_id>. If so abort the insert
+
+    if tolerance is not None:
+        start_time = event_time - tolerance
+        end_time = event_time + tolerance
+        get_events_catalog(api_base_url, start_time, end_time)
+
+    if event is not None:
+        logger.info('preparing event data')
         ev_io = BytesIO()
         event.write(ev_io, format='QUAKEML')
-        event_bytes = ev_io.getvalue()
+        event_bytes = ev_io
+        event_file_name = base_event_file_name + '.xml'
+        event_bytes.name = event_file_name
+        event_bytes.seek(0)
+        files['event'] = event_bytes
+        logger.info('done preparing event data')
 
     if stream is not None:
+        logger.info('preparing waveform data')
         st_io = BytesIO()
         stream.write(st_io, format='MSEED')
-        mseed_bytes = st_io.getvalue()
+        mseed_bytes = st_io
+        mseed_file_name = base_event_file_name + '.mseed'
+        mseed_bytes.name = mseed_file_name
+        mseed_bytes.seek[0]
+        files['waveform'] = mseed_bytes
+        logger.info('done preparing waveform data')
 
     if context_stream is not None:
+        logger.info('preparind context waveform data')
         st_c_io = BytesIO()
         context_stream.write(st_c_io, format='MSEED')
-        mseed_context_bytes = st_c_io.getvalue()
-
-    return build_request_data_from_bytes(event_id, event_bytes, mseed_bytes,
-                                         mseed_context_bytes)
-
-
-def build_request_data_from_bytes(event_id, event_bytes, mseed_bytes,
-                                  mseed_context_bytes):
-    files = dict()
-
-    # prepare event
-    if event_bytes is not None:
-        files['event'] = event_bytes
-
-    # prepare waveform
-    if mseed_bytes is not None:
-        files['waveform'] = mseed_bytes
-
-    # prepare context waveform
-    if mseed_context_bytes is not None:
+        mseed_context_bytes = st_c_io
+        mseed_context_file_name = base_event_file_name + '.context_mseed'
+        mseed_context_bytes.name = mseed_context_file_name
+        mseed_context_bytes.seek(0)
         files['context'] = mseed_context_bytes
+        logger.info('done preparind context waveform data')
 
-    data = dict()
-    data['event_resource_id'] = str(event_id)
-
-    print(data)
-
-    return data, files
+    return post_event_data(api_base_url, data, files)
 
 
 def post_event_data(api_base_url, request_data, request_files):
@@ -187,15 +247,32 @@ def get_events_catalog(api_base_url, start_time, end_time):
     return events
 
 
-def get_continuous_stream(api_base_url, start_time, end_time):
+def get_event_by_id(api_base_url, event_resource_id):
+    import json
+
+    # smi:local/e7021615-e7f0-40d0-ad39-8ff8dc0edb73
+    url = api_base_url + "events"
+    querystring = {"event_resource_id": event_resource_id}
+
+    response = requests.request("GET", url, params=querystring)
+
+    if response.status_code != 200:
+        return None
+
+    return RequestEvent(json.loads(response.content))
+
+
+def get_continuous_stream(api_base_url, start_time, end_time, station=None,
+                          channel=None, network=None):
     url = api_base_url + "continuous_waveform"
 
-    querystring = {"start_time": start_time, "end_time": end_time}
 
-    response = requests.request("GET", url,  params=querystring)
+    querystring = {'start_time': str(start_time), 'end_time': str(end_time),
+                   "station":station,}
+
+    response = requests.request('GET', url,  params=querystring)
     file = BytesIO(response.content)
     wf = read(file, format='MSEED')
-    print(wf)
     return wf
 
 
