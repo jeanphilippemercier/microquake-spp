cdb9bdd7ed261844a6c5d0178d13f0f00aef88b9
Jean-Philippe Mercier (OT) <Jean-PhilippeM@riotinto.com>
2019-02-12 13:09:07 +0800

    commented few lines related to load_inventory

--- spp/utils/application.py
+++ spp/utils/application.py
@@ -3,7 +3,7 @@ import os
 from microquake.core.util.attribdict import AttribDict
 from microquake.core.data.grid import create, read_grid
 from microquake.core.data.station import read_stations
-from microquake.core.data.station2 import load_inventory
+# from microquake.core.data.station2 import load_inventory
 import logging
 import sys
 from logging.handlers import TimedRotatingFileHandler
@@ -85,18 +85,18 @@ class Application(object):
         fpath = os.path.join(self.common_dir, fname)
         ttable.write_h5(fpath, ttp, tdict2=tts)
 
-    def get_inventory(self):
-        params = self.settings.sensors
-
-        if self.inventory is None:
-            print("app.get_inventory: Load inventory file")
-            if params.source == 'local':
-                fpath = os.path.join(self.common_dir, params.path)
-                self.inventory = load_inventory(fpath, format='CSV')
-            elif self.settings.sensors.source == 'remote':
-                pass
-
-        return self.inventory
+    # def get_inventory(self):
+    #     params = self.settings.sensors
+    #
+    #     if self.inventory is None:
+    #         print("app.get_inventory: Load inventory file")
+    #         if params.source == 'local':
+    #             fpath = os.path.join(self.common_dir, params.path)
+    #             self.inventory = load_inventory(fpath, format='CSV')
+    #         elif self.settings.sensors.source == 'remote':
+    #             pass
+    #
+    #     return self.inventory
 
     def get_stations(self):
         params = self.settings.sensors
