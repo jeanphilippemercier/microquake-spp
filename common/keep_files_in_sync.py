890f7ee8304b28c2ca75b1754889e0524394c287
Mike Hagerty <m.hagerty@isti.com>
2019-03-19 10:50:45 -0400

    Added code to track csv files - if these change then it will recreate stationXML (OT.xml) and nlloc station time grids

--- common/keep_files_in_sync.py
+++ common/keep_files_in_sync.py
@@ -34,85 +34,123 @@ from microquake.core.data.write_ot import write_OT_xml
     of the files here and recreate if they change
 """
 
+class file_holder():
+    pass
+
+
+# For now this is just a local file on disk:
 SHA_DB_FILE = "DB"
 
-if 'SPP_COMMON' not in os.environ or 'SPP_CONFIG' not in os.environ:
-    logger.error("Set your SPP envs!")
-    exit(2)
-path = os.environ['SPP_COMMON']
 
-sensor_file = os.path.join(path, 'sensors.csv')
-sensor_types_file = os.path.join(path,'sensor_types.csv')
-cables_file = os.path.join(path, 'cables.csv')
-
-# reading application data
-app = Application()
-settings = app.settings
-xml_outfile = os.path.join(path, settings.sensors.stationXML)
-
-#files_to_track = ['sensors.csv', 'cables.csv', 'sensor_types.csv', 'OT.xml']
-files_to_track = [sensor_file, sensor_types_file, cables_file, xml_outfile]
+def main():
 
 
-try:
-    with open(SHA_DB_FILE, 'rb') as handle:
-        l = pickle.load(handle)
-except IOError:
-    l = []
-
-db = dict(l)
+    if 'SPP_COMMON' not in os.environ or 'SPP_CONFIG' not in os.environ:
+        logger.error("Set your SPP envs!")
+        exit(2)
+    path = os.environ['SPP_COMMON']
 
 
-create_new_stationxml = False
-create_new_nllocgrids = False
+    sensor_file = os.path.join(path, 'sensors.csv')
+    sensor_types_file = os.path.join(path,'sensor_types.csv')
+    cables_file = os.path.join(path, 'cables.csv')
 
-for file in files_to_track:
+    # reading application data
+    app = Application()
+    settings = app.settings
+    xml_outfile = os.path.join(path, settings.sensors.stationXML)
 
-    checksum = hashlib.md5(open(file).read().encode('utf-8')).hexdigest()
-    print("file:%s checksum:%s" % (file, checksum))
+    files_to_track = [sensor_file, sensor_types_file, cables_file, xml_outfile]
 
-    if db.get(file, None) != checksum:
-        print("file:%s checksum:%s != db.checksum:%s" % (file, checksum, db.get(file,None)))
-
-        #db[file] = checksum
-        create_new_stationxml = True
-        create_new_nllocgrids = True
-
-    else:
-        print("file:%s HAS NOT changed" % file)
+    files = file_holder()
+    files.sensor_file = sensor_file
+    files.sensor_types_file = sensor_types_file
+    files.cables_file = cables_file
+    files.xml_outfile = xml_outfile
 
 
-if create_new_stationxml:
+    try:
+        with open(SHA_DB_FILE, 'rb') as handle:
+            l = pickle.load(handle)
+    except IOError:
+        l = []
+
+    db = dict(l)
+
+
+    create_new_stationxml = False
+    create_new_nllocgrids = False
+
+    for file in files_to_track:
+
+        checksum = hashlib.md5(open(file).read().encode('utf-8')).hexdigest()
+        print("file:%s checksum:%s" % (file, checksum))
+
+        if db.get(file, None) != checksum:
+            print("file:%s checksum:%s != db.checksum:%s" % (file, checksum, db.get(file,None)))
+
+            db[file] = checksum
+            create_new_stationxml = True
+            create_new_nllocgrids = True
+
+        else:
+            print("file:%s HAS NOT changed" % file)
+
+
+    if create_new_stationxml:
+        update_stationxml(files)
+        db[xml_outfile] = hashlib.md5(open(xml_outfile).read().encode('utf-8')).hexdigest()
+
+    if create_new_nllocgrids:
+        update_nllocgrids(app)
+
+    update_db(db)
+
+    return
+
+def update_stationxml(files):
 
     import datetime
     x = datetime.datetime.now()
     suff = x.strftime("%x_%X")
     suff = x.strftime("%Y-%m-%dT%H:%M:%S")
 
+    xml_outfile = files.xml_outfile
+
     import shutil
     shutil.copy(xml_outfile, xml_outfile + suff)
 
-    success = write_OT_xml(sensor_file, sensor_types_file, cables_file, xml_outfile=xml_outfile, logger_in=logger)
+    success = write_OT_xml(files.sensor_file, files.sensor_types_file,
+                           files.cables_file, xml_outfile=files.xml_outfile,
+                           logger_in=logger)
 
     assert success == 1
 
-    db[xml_outfile] = hashlib.md5(open(xml_outfile).read().encode('utf-8')).hexdigest()
+    return
 
-if create_new_nllocgrids:
-    print("create_new_nllocgrids is True but let's skip!")
+def update_nllocgrids(app):
     from microquake.nlloc import NLL
-    params = app.settings.nlloc
-    logger.info('Preparing NonLinLoc')
+    settings = app.settings
+    params = settings.nlloc
 
     project_code = settings.project_code
     base_folder = settings.nlloc.nll_base
     gridpar = app.nll_velgrids()
     sensors = app.nll_sensors()
 
-    #nll = NLL(project_code, base_folder=base_folder, gridpar=gridpar, sensors=sensors, params=params)
-    #nll.prepare(create_time_grids=True)
+    logger.info('Preparing NonLinLoc')
 
-print("Update DB FILE")
-with open(SHA_DB_FILE, 'wb') as handle:
-    pickle.dump(db, handle, protocol=pickle.HIGHEST_PROTOCOL)
+    nll = NLL(project_code, base_folder=base_folder, gridpar=gridpar, sensors=sensors, params=params)
+    nll.prepare(create_time_grids=True)
+
+def update_db(db):
+    with open(SHA_DB_FILE, 'wb') as handle:
+        pickle.dump(db, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
+    return
+
+
+if __name__ == "__main__":
+    main()
+
 
