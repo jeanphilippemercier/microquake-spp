52cfc515ee4656b64d0e97832d5e0cc2e534cb11
Zach Goldstein <zachgold@gmail.com>
2019-03-25 14:22:23 -0400

    Add azimuth and takeoff_angle to API client post_rays function

--- spp/utils/seismic_client.py
+++ spp/utils/seismic_client.py
@@ -355,7 +355,7 @@ def post_continuous_stream(api_base_url, stream, post_to_kafka=True,
 
 
 def post_ray(api_base_url, site_code, network_code, event_id, origin_id,
-             arrival_id, station_code, phase, ray_length, travel_time, nodes):
+             arrival_id, station_code, phase, ray_length, travel_time, azimuth, takeoff_angle, nodes):
     url = api_base_url + "rays"
 
     request_data = dict()
@@ -368,6 +368,8 @@ def post_ray(api_base_url, site_code, network_code, event_id, origin_id,
     request_data['phase'] = phase
     request_data['ray_length'] = str(ray_length)
     request_data['travel_time'] = str(travel_time)
+    request_data['azimuth'] = azimuth
+    request_data['takeoff_angle'] = takeoff_angle
     request_data['nodes'] = nodes.tolist()
 
     result = requests.post(url, json=request_data)
