92797f02603f5309079ae11b65d0b570797d34b2
Reza Jelveh <toyb0mb@gmx.net>
2019-04-23 17:02:21 +0800

    testing: load spp_config / common in conftest.py fixture and add settings tests

---
+++ tests/conftest.py
@@ -0,0 +1,15 @@
+import os
+
+import pytest
+
+import spp.core.settings as sppsettings
+
+
+@pytest.fixture(scope="session", autouse=True)
+def execute_before_any_test():
+    os.environ['SPP_COMMON'] = os.path.join(os.getcwd() + '/common')
+
+@pytest.fixture(scope="module", autouse=True)
+def settings():
+  sppsettings.settings.load()
+  return sppsettings.settings
---
+++ tests/test_settings.py
@@ -0,0 +1,15 @@
+import logging
+import os
+
+import pytest
+
+
+def test_common_directory(settings):
+    common_dir = os.path.join(os.getcwd() + '/common')
+    assert common_dir == settings.common_dir
+
+def test_nll_base(settings):
+    assert 'NLL' == settings.nll_base
+
+def test_project_code(settings):
+    assert 'OT' == settings.PROJECT_CODE
