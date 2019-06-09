393ff7e512230a970c537f5164686bc4941c9100
Reza Jelveh <toyb0mb@gmx.net>
2019-06-10 03:52:54 +0800

    refactor: add de/serializer class

---
+++ spp/core/serializers/serializer.py
@@ -0,0 +1,33 @@
+from ..utils import timing
+from ..settings import settings
+
+
+class Serializer():
+    def __init__(self, msg_in):
+        self.message = msg_in
+        self.deserialize()
+
+    @property
+    def eventid(self):
+        return str(self.catalog[0].resource_id)
+
+    @property
+    def waveform(self):
+        return self.clean_waveform_stream()
+
+    @property
+    def catalog(self):
+        return self.catalog.copy()
+
+    def clean_waveform_stream(self):
+        black_list = settings.sensors.black_list
+        waveforms = self.waveform_stream.copy()
+
+        if black_list is None:
+            return
+
+        for trace in waveforms:
+            if trace.stats.station in black_list:
+                waveforms.remove(trace)
+
+        return waveforms
