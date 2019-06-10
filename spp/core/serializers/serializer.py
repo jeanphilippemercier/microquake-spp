43331fc355326aac6139d4e69007f3072ce65c23
jpmercier <jpmercier01@gmail.com>
2019-06-10 20:05:34 +0800

    adding a serializer function

--- spp/core/serializers/serializer.py
+++ spp/core/serializers/serializer.py
@@ -1,32 +1,60 @@
-from ..settings import settings
+from redis import StrictRedis
+from io import BytesIO
+from microquake.core import read, read_events
+import pickle
 
 
-class Serializer():
-    def __init__(self, msg_in):
-        self.message = msg_in
-        self.deserialize()
+class Seismic():
+    def __init__(self, redis_key, redis_settings, types):
+        """
 
-    @property
-    def eventid(self):
-        return str(self.catalog[0].resource_id)
+        :param redis_key: base redis key used to insert or read the data
+        :param redis_settings: setting to connect to the redis database
+        :param types: list of types to serialize or deserialize
+        """
+        self.accepted_types = ['fixed_length', 'variable_length', 'context',
+                          'continuous', 'catalog']
 
-    @property
-    def waveform(self):
-        return self.clean_waveform_stream()
+        if type(types) is str:
+            if types not in self.accepted_types:
+                raise ValueError('waveform_type must be in %s' %
+                                 self.accepted_types)
+            self.types = [types]
 
-    @property
-    def catalog(self):
-        return self.catalog.copy()
+        elif type(types) is list:
+            for otype in types:
+                if otype not in self.accepted_types:
+                    raise ValueError('waveform_type must be in %s' %
+                                     self.accepted_types)
+            self.types = types
 
-    def clean_waveform_stream(self):
-        black_list = settings.sensors.black_list
-        waveforms = self.waveform_stream.copy()
+        else:
+            raise TypeError('types must either be a <str> or a '
+                            '<list>')
 
-        if black_list is None:
-            return
+        self.redis_conn = StrictRedis(**redis_settings)
+        self.redis_base_key = redis_key
 
-        for trace in waveforms:
-            if trace.stats.station in black_list:
-                waveforms.remove(trace)
+    def serialize(self, input_dictionary):
+        for key in input_dictionary.keys():
+            if key not in self.types:
+                raise ValueError('%s is not a valid <type>! \n '
+                                 'valid types are %s'
+                                 % (key, self.accepted_types))
 
-        return waveforms
+            redis_key = '%s.%s' % (self.redis_base_key, key)
+
+            self.redis_conn.set(redis_key,
+                                pickle.dumps(input_dictionary[key]))
+            # from pdb import set_trace; set_trace()
+
+
+    def deserialize(self):
+        output_dict = {}
+        for key in self.types:
+            redis_key = '%s.%s' % (self.redis_base_key, key)
+            output_dict[key] = pickle.loads(self.redis_conn.get(redis_key))
+
+        return output_dict
+
+
