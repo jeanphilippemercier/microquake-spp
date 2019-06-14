3d55b33764e0b91a4ff0e349d2ed85305d8ba3fd
jpmercier <jpmercier01@gmail.com>
2019-06-14 15:04:42 +0800

    greatly simplified the serializer

--- spp/faust_apps/serializer.py
+++ spp/faust_apps/serializer.py
@@ -1,59 +1,33 @@
-from redis import Redis
+from redis import StrictRedis
 import pickle
 
 
 class Seismic:
-    def __init__(self, redis_key, redis_settings, types, ttl=1200):
+    def __init__(self, redis_settings):
         """
 
         :param redis_key: base redis key used to insert or read the data
         :param redis_settings: setting to connect to the redis database
-        :param types: list of types to serialize or deserialize
         :param ttl: time to leave for object inserted in Redis
         """
-        self.accepted_types = ['fixed_length', 'variable_length', 'context',
-                               'continuous', 'catalog']
 
-        self.ttl = ttl
+        self.redis_conn = StrictRedis(**redis_settings)
 
-        if type(types) is str:
-            if types not in self.accepted_types:
-                raise ValueError('waveform_type must be in %s' %
-                                 self.accepted_types)
-            self.types = [types]
-
-        elif type(types) is list:
-            for otype in types:
-                if otype not in self.accepted_types:
-                    raise ValueError('waveform_type must be in %s' %
-                                     self.accepted_types)
-            self.types = types
-
-        else:
-            raise TypeError('types must either be a <str> or a '
-                            '<list>')
-
-        self.redis_conn = Redis(**redis_settings)
-        self.redis_base_key = redis_key
-
-    def serialize(self, input_dictionary):
+    def serialize(self, process_id: str, input_dictionary: dict):
         for key in input_dictionary.keys():
-            if key not in self.types:
-                raise ValueError('%s is not a valid <type>! \n'
-                                 'valid types are %s'
-                                 % (key, self.accepted_types))
 
-            redis_key = '%s.%s' % (self.redis_base_key, key)
+            redis_key = '%s.%s' % (process_id, key)
 
             self.redis_conn.set(redis_key,
-                                pickle.dumps(input_dictionary[key]),
-                                ex=self.ttl)
+                                pickle.dumps(input_dictionary[key]))
             # from pdb import set_trace; set_trace()
 
-    def deserialize(self):
+    def deserialize(self, process_id: str, output_keys: list):
         output_dict = {}
-        for key in self.types:
-            redis_key = '%s.%s' % (self.redis_base_key, key)
+        for key in output_keys:
+            redis_key = '%s.%s' % (process_id, key)
             output_dict[key] = pickle.loads(self.redis_conn.get(redis_key))
 
         return output_dict
+
+
