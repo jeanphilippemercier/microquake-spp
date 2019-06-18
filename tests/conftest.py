2741bd3dc5a8bc343da405cf2e640f2759ca5364
Reza Jelveh <toyb0mb@gmx.net>
2019-06-19 02:11:35 +0800

    remove SPP_COMMON test

--- tests/conftest.py
+++ tests/conftest.py
@@ -8,9 +8,9 @@ import spp.core.settings as sppsettings
 sys.path.append(os.path.join(os.path.dirname(__file__), 'helpers'))
 
 
-@pytest.fixture(scope="session", autouse=True)
-def execute_before_any_test():
-    os.environ['SPP_COMMON'] = os.path.join(os.getcwd() + '/common')
+# @pytest.fixture(scope="session", autouse=True)
+# def execute_before_any_test():
+#     os.environ['SPP_COMMON'] = os.path.join(os.getcwd() + '/common')
 
 
 @pytest.fixture(scope="module", autouse=True)
