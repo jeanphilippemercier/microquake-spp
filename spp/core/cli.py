7d4652ac344b7817c0713df74d6976e87ea16b7b
Reza Jelveh <toyb0mb@gmx.net>
2019-06-27 02:11:05 +0800

    add velocity model generation to cli

--- spp/core/cli.py
+++ spp/core/cli.py
@@ -24,6 +24,7 @@ import click
 import msgpack
 
 from loguru import logger
+from microquake.nlloc import NLL
 from redis import Redis
 
 from .. import __version__
@@ -31,7 +32,10 @@ from ..pipeline.automatic_pipeline import automatic_pipeline
 from ..pipeline.manual_pipeline import manual_pipeline
 # from ..pipeline.manual_pipeline import manual_pipeline
 from ..utils.kafka_redis_application import KafkaRedisApplication
+from .hdf5 import write_ttable_h5
+from .nlloc import nll_sensors, nll_velgrids
 from .settings import settings
+from .velocity import create_velocities
 
 LOGGING_LEVELS = {
     0: logging.NOTSET,
@@ -210,3 +214,36 @@ def version():
     Get the library version.
     """
     click.echo(click.style(f'{__version__}', bold=True))
+
+
+@cli.command()
+def prepare():
+    """
+    Prepare project and run NonLinLoc
+    """
+    project_code = settings.PROJECT_CODE
+    base_folder = settings.nll_base
+    gridpar = nll_velgrids()
+    sensors = nll_sensors()
+    params = settings.get('nlloc')
+
+    nll = NLL(project_code, base_folder=base_folder, gridpar=gridpar,
+              sensors=sensors, params=params)
+
+    # creating NLL base project including travel time grids
+    logger.info('Preparing NonLinLoc')
+    nll.prepare()
+    logger.info('Done preparing NonLinLoc')
+
+    # creating H5 grid from NLL grids
+    logger.info('Writing h5 travel time table')
+    write_ttable_h5()
+
+
+@cli.command()
+def velocities():
+    """
+    Create velocity files
+    """
+    logger.info('Create velocity model')
+    create_velocities()
