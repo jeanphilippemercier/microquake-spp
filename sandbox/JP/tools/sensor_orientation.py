1e1bf165cefd6cdb25681a036e25860408c6d9f1
Jean-Philippe Mercier <jean-philippem@riotinto.com>
2019-09-23 01:49:40 -0700

    modified the function to calculate the orientation

--- sandbox/JP/tools/sensor_orientation.py
+++ sandbox/JP/tools/sensor_orientation.py
@@ -1,32 +1,37 @@
 from datetime import datetime
 from obspy import UTCDateTime
 from microquake.core import read
-from microquake.clients import api_client
-from spp.utils.application import Application
 import numpy as np
-import pickle
-import logging
 from tqdm import tqdm
 from pathos.multiprocessing import ProcessingPool
-ev_info_logger = logging.getLogger('get_event_information')
+from microquake.core.settings import settings
+from microquake.core.helpers.time import get_time_zone
+from microquake.core.helpers.grid import get_grid
+from loguru import logger
+from microquake.clients import api_client
+from time import time
+import pickle
 
 
 def get_event_information(request_event, output_dir='data/'):
     event = request_event
     t0 = time()
-    ev_info_logger.info('requesting data for event %s' % event.time_utc)
+    logger.info('requesting data for event %s' % event.time_utc)
     cat = event.get_event()
     st = event.get_waveforms()
     t1 = time()
-    ev_info_logger.info('done requesting data for event %s in %0.3f seconds' %
+    logger.info('done requesting data for event %s in %0.3f seconds' %
                         (event.time_utc, (t1 - t0)))
     dict_seismo = {}
-    for sta in st.unique_stations():
-        seismo = st.select(station=sta)
-        filename = output_dir + '%s_%s.mseed' % (sta, event.time_utc)
+
+    for station in st.unique_stations():
+
+        seismo = st.select(station=station)
+        filename = output_dir + '%s_%s.mseed' % (station, event.time_utc)
         filename = filename.replace(r':', '_').replace('-', '_')
-        dict_seismo[sta] = filename
+        dict_seismo[station] = filename
         seismo.write(filename)
+
     for arrival in cat[0].preferred_origin().arrivals:
         pick = arrival.get_pick()
         if pick.phase_hint == 'S':
@@ -37,7 +42,7 @@ def get_event_information(request_event, output_dir='data/'):
         yield (key, (seismogram_name, p_time, cat[0].preferred_origin().loc))
 
 
-def get_traveltime_grid_station(station, phase='P'):
+def get_travel_time_grid_station(station, phase='P'):
     """
     return the travel time grid for a specific station and a specific phase
     :param station: station name
@@ -49,33 +54,11 @@ def get_traveltime_grid_station(station, phase='P'):
     # To be changed, need to use the NLL grids instead of the microquake grids
     ###
 
-    params = get_project_params()
-    base_dir = params.nll['NLL_BASE']
-    project_code = params.project_code
+    st_loc = inventory.select(station)[0].loc
 
+    tt_grid = get_grid(station, phase.upper())
 
-    from microquake.core import read_grid
-
-    site = get_stations()
-    stloc = site.select(station=station).stations()[0].loc
-
-    try:
-        phase = phase.lower()
-
-        filename = '%s/time/%s.%s.%s.time' % (base_dir, project_code, phase,
-                                                  station)
-
-        tt_grid = read_grid(filename, format='NLLOC')
-
-    except:
-        phase = phase.upper()
-
-        filename = '%s/time/%s.%s.%s.time' % (base_dir, project_code, phase,
-                                                  station)
-
-        tt_grid = read_grid(filename, format='NLLOC')
-
-    tt_grid.seed = stloc
+    tt_grid.seed = st_loc
 
     return tt_grid
 
@@ -104,35 +87,32 @@ def sv_sh_orientation(P):
 
     return (SV_vect, SH_vect)
 
-def calculate_orientation_station(station, pick_dict, site, logger):
 
-    base_url = "http://10.95.64.12:8002/ims-database-server/databases/mgl"
-    network_code = 'OT'
+def calculate_orientation_sensor(station, pick_dict, inventory):
+
     pick_array = pick_dict[station]
 
-    sta = site.select(station=station).stations()[0]
+    station = inventory.select(station)
 
-    try:
-        zorientation = sta.channels[2].orientation
-    except:
-        return (0, 0, 0)
-    stloc = sta.loc
+    if len(station.channels) < 3:
+        return station.channels[0].cosines
 
-    N = 100
+    z_orientation = station[2].cosines
 
-    Res = np.zeros(N ** 2)
+    st_loc = station.loc
+
+    n = 100
+
+    res = np.zeros(n ** 2)
 
     for kk, pick in enumerate(tqdm(pick_array)):
         logger.debug("processing station %s, event %d of %d, "
                      "" % (station, kk, len(pick_array)))
-        evloc = pick[2]
-        sgram_name = pick[0]
+        ev_loc = pick[2]
         pk_time = UTCDateTime(pick[1])
-        # st = web_api.get_seismogram(base_url, sgram_name, network_code, station)
         st = read(pick[0], format='MSEED')
         st.detrend('demean').detrend('linear')
         st.filter('bandpass', freqmin=100, freqmax=1000)
-        st2 = st.copy()
         starttime = pk_time
         endtime = pk_time + 0.015
         st.trim(starttime=starttime, endtime=endtime)
@@ -143,29 +123,25 @@ def calculate_orientation_station(station, pick_dict, site, logger):
         i_mx_y = np.argmax(np.abs(st[1].data))
         i_mx_z = np.argmax(np.abs(st[2].data))
 
-        A = np.sqrt(st[0].data[i_mx_x] ** 2 + st[1].data[i_mx_y] ** 2 + st[
+        a = np.sqrt(st[0].data[i_mx_x] ** 2 + st[1].data[i_mx_y] ** 2 + st[
             2].data[i_mx_z] ** 2)
-        # A_x = st[0].data[i_mx_x]
-        # A_y = st[1].data[i_mx_y]
-        # A_z = st[2].data[i_mx_z]
-
-        A_x = np.std(st[0].data)
-        A_y = np.std(st[1].data)
+        a_x = np.std(st[0].data)
+        a_y = np.std(st[1].data)
         cc_x_z = np.sign(np.corrcoef(st[0].data, st[2].data)[0, 1])
         cc_y_z = np.sign(np.corrcoef(st[1].data, st[2].data)[0, 1])
 
-        x = np.linspace(-1, 1, N)
-        y = np.linspace(-1, 1, N)
+        x = np.linspace(-1, 1, n)
+        y = np.linspace(-1, 1, n)
 
         X, Y = np.meshgrid(x, y)
 
-        Xx = X.reshape(N ** 2)
-        Yx = Y.reshape(N ** 2)
+        Xx = X.reshape(n ** 2)
+        Yx = Y.reshape(n ** 2)
 
-        X_Xz = Xx * zorientation[0]
-        Y_Yz = Yx * zorientation[1]
+        X_Xz = Xx * z_orientation[0]
+        Y_Yz = Yx * z_orientation[1]
 
-        Zx = - (X_Xz + Y_Yz) / zorientation[2]
+        Zx = - (X_Xz + Y_Yz) / z_orientation[2]
 
         Nx = np.sqrt(Xx ** 2 + Yx ** 2 + Zx ** 2)
 
@@ -180,30 +156,15 @@ def calculate_orientation_station(station, pick_dict, site, logger):
 
         X = np.array([([x_x, y_x, z_x] ) for (x_x, y_x, z_x) in zip(Xx, Yx,
                                                                     Zx)])
-        z = zorientation
+        z = z_orientation
         # The sensors are right handed coordinate system
         Y = - np.array([np.cross(x, z) for x in X])
 
-        # reload(eik)
-        #
-        # ray = eik.ray_tracer(tt_grid, evloc, stloc)
-        #
-        # # finding the direction of the incident wave
-        # inc_vects = np.diff(ray.nodes, axis=0)
-        # inc_vect_norm = [inc_vect / np.linalg.norm(inc_vect) for inc_vect
-        #                  in inc_vects]
-        # inc_vect = inc_vect_norm[-2] # the last vector is reliable
-
-        inc_vect = (stloc - evloc) / (np.linalg.norm(stloc - evloc))
+        inc_vect = (st_loc - ev_loc) / (np.linalg.norm(st_loc - ev_loc))
         SV_vect, SH_vect = sv_sh_orientation(inc_vect)
 
         dot_inc_X = np.array([np.dot(inc_vect, x) for x in X])
         dot_inc_Y = np.array([np.dot(inc_vect, y) for y in Y])
-
-        # res_x = dot_inc_X / (A_x / A * np.sign(np.dot(z, inc_vect)) *
-        #                          cc_x_z)
-        # res_y = dot_inc_Y / (A_y / A * np.sign(np.dot(z, inc_vect)) *
-        #                          cc_y_z)
 
         P_ = [np.var(np.dot(x, inc_vect) * st[0].data + np.dot(y, inc_vect) *
              st[1].data + np.dot(z, inc_vect) * st[2].data)
@@ -220,17 +181,13 @@ def calculate_orientation_station(station, pick_dict, site, logger):
         P_ = np.array(P_)
         SV_ = np.array(SV_)
         SH_ = np.array(SH_)
-        # P_ = P_ / np.max(P_)
 
-        # res = np.abs(res_x) + np.abs(res_y) + (SV_ + SH_) / P_
-        #
-        # res = (SV_ + SH_) / P_
         res = (SV_ + SH_) / P_
         res = 1 / res
-        Res += res
+        res += res
 
         i = np.argmax(res)
-        i2 = np.argmax(Res)
+        i2 = np.argmax(res)
 
         x = X[i, :]
         x2 = X[i2, :]
@@ -242,127 +199,72 @@ def calculate_orientation_station(station, pick_dict, site, logger):
         logger.debug("y1: %s" % y)
         logger.debug("y1: %s" % y2)
         logger.debug(res[i])
-        logger.debug(Res[i2])
+        logger.debug(res[i2])
 
-
-
-        # P = np.dot(x, inc_vect) * st2[0].data + np.dot(y, inc_vect) * st2[
-        #     1].data + np.dot(z, inc_vect) * st2[2].data
-        #
-        #
-        # # x = np.array([0.98039421, 0.19607884, 0.01950084])
-        # # y = np.array([ 1.96228326e-01, -9.80539542e-01, -6.05393413e-03])
-        #
-        # SH = np.dot(x, SH_vect) * st2[0].data + np.dot(y, SH_vect) * st2[
-        #      1].data + np.dot(z, SH_vect) * st2[2].data
-        #
-        # SV = np.dot(x, SV_vect) * st2[0].data + np.dot(y, SV_vect) * st2[
-        #      1].data + np.dot(z, SV_vect) * st2[2].data
-        #
-        # import matplotlib.pyplot as plt
-        #
-        # # eig = np.linalg.eig([st[0].data,
-        # #                      st[1].data,
-        # #                      st[2].data])
-        #
-        # plt.close()
-        # plt.figure(1)
-        # plt.clf()
-        # plt.subplot(311)
-        # plt.plot(P)
-        # plt.subplot(312)
-        # plt.plot(SV)
-        # plt.subplot(313)
-        # plt.plot(SH)
-        # plt.show()
-        #
-        # X_vects.append(x)
-        # Y_vects.append(y)
-        # Z_vects.append(z)
-        #
-        # st.trim(starttime=pk_time, endtime=pk_time+0.015)
-        #
-        # P = np.dot(x, inc_vect) * st[0].data + np.dot(y, inc_vect) * st[
-        #     1].data + np.dot(z, inc_vect) * st[2].data
-        #
-        # SH = np.dot(x, SH_vect) * st[0].data + np.dot(y, SH_vect) * st[
-        #      1].data + np.dot(z, SH_vect) * st[2].data
-        #
-        # SV = np.dot(x, SV_vect) * st[0].data + np.dot(y, SV_vect) * st[
-        #      1].data + np.dot(z, SV_vect) * st[2].data
-        #
-        #
-        # print((np.var(SH) + np.var(SV)) / np.var(P))
-        #
-        # S_in_P.append((np.var(SH) + np.var(SV)) / np.var(P))
-        #
-        #
-        #
-        # # Tracer()()
-
-    i = np.argmax(Res)
+    i = np.argmax(res)
     x = X[i, :]
     y = Y[i, :]
 
     return x, y, z, station, len(pick_array)
 
 
-from time import time
-app = Application()
+# TODO This should be turned into a function that takes
+# 1) a start time
+# 2) an end time
+# 3) The list for sensor for which the orientation need to be calculated
+# The orientation of the sensors is given by the orientation dictionary
 
+inventory = settings.inventory
 
-site = app.get_stations()
-tz = app.get_time_zone()
-starttime = datetime(2018, 4, 1, tzinfo=tz)
+tz = get_time_zone()
+starttime = datetime(2018, 11, 29, tzinfo=tz)
 endtime = datetime(2018, 11, 30, tzinfo=tz)
-base_url = app.settings.seismic_api.base_url
+base_url = settings.get('API_BASE_URL')
 
-# cat = web_api.get_catalogue(base_url, starttime, endtime, site, blast=True,
-#                             get_arrivals=True)
-logger = app.get_logger('sensor_orientation', 'sensor_orientation.log')
 logger.info('requesting event list from the API')
 t0 = time()
 event_list = api_client.get_events_catalog(base_url, starttime, endtime)
 t1 = time()
-logger.info('done requesting event list. The API returned %d events in %0.3f seconds' % (len(event_list), (t1 - t0)))
-# cat.write('events.xml', format='QUAKEML')
-
-# read event data and write into H5 file
-
+logger.info('done requesting event list. '
+            'The API returned %d events in %0.3f seconds'
+            % (len(event_list), (t1 - t0)))
 
 print('done with reading the data')
 
-# pick_dict = {}
-# for tmp1 in map(get_event_information, tqdm(event_list)):
-#     for tmp2 in tmp1:
-#         if tmp2[0] in pick_dict.keys():
-#             pick_dict[tmp2[0]].append(np.array(tmp2[1]))
-#         else:
-#             pick_dict[tmp2[0]] = [np.array(tmp2[1])]
-#
-# pickle.dump(pick_dict, open('pick_dict.pickle', 'wb'))
+logger.info('building picking dictionary')
+pick_dict = {}
+for tmp1 in map(get_event_information, tqdm(event_list)):
+    for tmp2 in tmp1:
+        if tmp2[0] in pick_dict.keys():
+            pick_dict[tmp2[0]].append(np.array(tmp2[1]))
+        else:
+            pick_dict[tmp2[0]] = [np.array(tmp2[1])]
 
-pick_dict = pickle.load(open('pick_dict.pickle', 'rb'))
-# input('aqui')
-
-try:
-    orientation = pickle.load(open('orientation.pickle', 'rb'))
-
-except:
     orientation = {}
 
 
-# p = Pool(16)
 pool = ProcessingPool(nodes=10)
 
-fun = lambda x: calculate_orientation_station(x, pick_dict, site, logger)
-res = pool.map(fun, pick_dict.keys())
+# TODO replace sensor_id_list by the list provided by the user, note that if
+#  the list is empty, the orientation is calculated for all sensors
+sensor_id_list = []
+fun = lambda x: calculate_orientation_sensor(x, pick_dict, inventory)
 
+sensor_ids = []
+if not sensor_id_list:
+    sensor_ids = pick_dict.keys()
+else:
+    sensor_ids = [s_id for s_id in pick_dict.keys() if s_id in sensor_id_list]
 
-for r in res:
+results = pool.map(fun, sensor_ids)
+
+# Orientation is the orientation vectors for all the stations.
+# TODO
+for r in results:
     try:
-        sta = r[3]
+        sta = r[3].code
         orientation[sta] = {}
+        # the following are
         orientation[sta]['x'] = r[0]
         orientation[sta]['y'] = r[1]
         orientation[sta]['z'] = r[2]
@@ -370,51 +272,4 @@ for r in res:
     except:
         continue
 
-with open('orientation.pickle', 'wb') as fo:
-    pickle.dump(orientation, fo)
-
-
-st_ids = np.sort([int(station.code) for station in app.get_stations(
-                  ).stations()])
-
-with open('orientation.csv', 'w') as fo:
-    for st_id in st_ids:
-        key = str(st_id)
-        if not key in orientation.keys():
-            fo.write("%s,z,0,0,1\n" % st_id)
-            continue
-        print(key)
-        for k, station in enumerate(site.networks[0].stations):
-            if site.networks[0].stations[k].code == key:
-                if len(site.networks[0].stations[k].channels) == 1:
-                    z = site.networks[0].stations[k].channels[0]
-                    fo.write("%s,z,%f,%f,%f\n" % (st_id, z.orientation[0],
-                                          z.orientation[1], z.orientation[2]))
-                else:
-                    site.networks[0].stations[k].channels[0].orientation =  \
-                        orientation[key]['x']
-                    site.networks[0].stations[k].channels[1].orientation = \
-                        orientation[key]['y']
-                    x = site.networks[0].stations[k].channels[0].orientation
-                    y = site.networks[0].stations[k].channels[1].orientation
-                    z = site.networks[0].stations[k].channels[2].orientation
-                    fo.write("%s,x,%f,%f,%f,y,%f,%f,%f,z,%f,%f,%f\n" \
-                             % (st_id, x[0], x[1], x[2],
-                                y[0], y[1], y[2], z[0], z[1], z[2]))
-
-
-
-
-
-# next need to write in a file.
-
-
-# st = web_api.get_seismogram(base_url, sgram_name, network_code, site_code)
-
-
-# sc = SparkContext('local', 8)
-
-# rdd = sc.parallelize(cat)
-
-#st = web_api.get_seismogram_event(base_url, cat[10], 'OT')
-
+# TODO need to remove all the temporary files
