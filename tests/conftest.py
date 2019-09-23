b238a8bf742d92718f97670277c2c80596e268ea
Reza Jelveh <toyb0mb@gmx.net>
2019-09-23 19:43:05 +0800

    move tests to microquake

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,24 +4,6 @@ import microquake.core.settings as sppsettings
 import pytest
 from fakeredis import FakeStrictRedis
 
-from .helpers.data_utils import get_test_data
-
-pytest.test_data_name = None
-
-
-@pytest.fixture(scope="function")
-def catalog():
-    file_name = pytest.test_data_name + ".xml"
-
-    return get_test_data(file_name, "QUAKEML")
-
-
-@pytest.fixture(scope="function")
-def waveform_stream():
-    file_name = pytest.test_data_name + ".mseed"
-
-    return get_test_data(file_name, "MSEED")
-
 
 @pytest.fixture(scope="session", autouse=True)
 def redis():
