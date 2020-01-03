e6e6301c9eb0184b6b397285ee6aa34a505cc167
Jean-Philippe Mercier <jean-philippem@riotinto.com>
2020-01-02 23:59:09 -0800

    updating microquake version to 0.1.214

--- batch/reprocess_range.py
+++ batch/reprocess_range.py
@@ -1,80 +1,41 @@
-from microquake.pipelines import automatic_pipeline
 from microquake.clients.api_client import (get_events_catalog,
                                            put_data_from_objects)
-
-# from microquake.clients.api_client import get_event_types
-from microquake.clients.ims import web_client
 from microquake.core.settings import settings
-from microquake.processors import (interloc, quick_magnitude, ray_tracer,
-                                   event_classifier)
-from spp.data_connector.pre_processing import event_classification, get_event_types
-from obspy.core.event import ResourceIdentifier
-from datetime import datetime
+from microquake.core.helpers.time import get_time_zone
+from datetime import datetime, timedelta
+from obspy.core import UTCDateTime
+from microquake.pipelines.pipeline_meta_processors import \
+    magnitude_meta_processor
 from loguru import logger
-from microquake.core.event import Catalog, Event
+from urllib.parse import urlencode
 import requests
 
-api_base_url = settings.get('API_BASE_URL')
+tz = get_time_zone()
 
-start_time = datetime(2019, 3, 31)
-end_time = datetime(2019, 11, 1)
+# months = ['04', '05', '06', '07', '08', '09', '10', '11']
 
-inventory = settings.inventory
+# for month in ['04', '05', '06', '07', '08', '09', '10', '11']:
+query = urlencode({'time_utc_after': f'2019-04-01T00:00:00Z',
+                   'time_utc_before': f'2019-04-05T00:00:00Z',
+                   'event_type': 'earthquake',
+                   'status': 'accepted'})
 
-res = get_events_catalog(api_base_url, start_time, end_time,
-                         event_type='earthquake')
+base_url = settings.get('api_base_url')
+url = base_url + 'events'
+r = requests.patch(f'{url}?{query}', json={'send_to_bus': True})
+print(r)
 
-event_types_lookup = get_event_types()
-# event_types_lookup = get_event_types(api_base_url)
+# re = get_events_catalog(base_url, starttime, endtime, event_type='earthquake')
+#
+# for event in re:
+#     logger.info(f'processing event {event.event_resource_id}')
+#     logger.info('getting the event catalogue')
+#     cat = event.get_event().copy()
+#     logger.info('getting the waveforms')
+#     fixed_length = event.get_waveforms()
+#
+#     cat_out = magnitude_meta_processor(cat, fixed_length)
+#     put_data_from_objects(base_url, event.network, cat=cat_out.copy())
 
-network = settings.network_code
 
-nb_event = len(res)
-for i in range(len(res)):
-    re = res[i]
-    event_id = re.event_resource_id
-    logger.info(event_id)
 
-    percent = (i + 1) / len(res) * 100
-
-    logger.info('{:0.1f}% - event {} of {}: {}'.format(percent, i+1,
-                                                       nb_event, event_id))
-
-    try:
-        logger.info('downloading catalog')
-        cat = re.get_event()
-        cat.origins = []
-        cat.magnitudes = []
-        logger.info('downloading waveforms')
-        st = re.get_waveforms()
-        context = re.get_context_waveforms()
-        vl = re.get_variable_length_waveforms()
-    except KeyboardInterrupt:
-        exit()
-    except:
-        continue
-
-    # make sur the channel is upper case.
-    for tr in st:
-        tr.stats.channel = tr.stats.channel.upper()
-    
-    interloc_processor = interloc.Processor()
-    tmp = interloc_processor.process(stream=st)
-    cat_interloc = interloc_processor.output_catalog(cat.copy())
-    magnitude_processor = quick_magnitude.Processor()
-    tmp = magnitude_processor.process(cat=cat_interloc.copy(),
-                                      stream=st)
-    cat_magnitude = magnitude_processor.output_catalog(cat_interloc.copy())
-
-    cat_classified = event_classification(cat_magnitude.copy(), st, context, 
-                                          event_types_lookup)[0]
-   
-    ray_tracer_processor = ray_tracer.Processor()
-    tmp = ray_tracer_processor.process(cat=cat_classified.copy())
-    cat_ray_trace = ray_tracer_processor.output_catalog(cat_classified.copy())
-   
-    cat_auto = automatic_pipeline.automatic_pipeline(cat_ray_trace.copy(), st)
-    logger.info('sending data to the api')
-
-    response = put_data_from_objects(api_base_url, network, cat=cat_auto)
-    print(response)
