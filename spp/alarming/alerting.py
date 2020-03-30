924096f3ba69d6a7af41633e99a3c705651e61ec
jpmercier <jpmercier01@gmail.com>
2020-03-29 20:37:20 -0400

    alerting module

---
+++ spp/alarming/alerting.py
@@ -0,0 +1,288 @@
+import smtplib
+from microquake.core.settings import settings
+from microquake.clients.api_client import get_catalog
+import requests
+from dateutil.parser import parse
+from datetime import datetime, timedelta
+from pytz import utc
+# from spp.alerting.alert_db_helpers import (create_postgres_session,
+#                                            AlarmingState)
+
+# from .alert_db_helpers import (create_postgres_session, AlarmingState)
+from alert_db_helpers import (create_postgres_session, AlarmingState)
+import pandas as pd
+from sklearn.cluster import MeanShift
+from loguru import logger
+import numpy as np
+from sqlalchemy import desc
+
+ms_host = settings.get('MAIL_SERVER_HOST')
+ms_port = settings.get('MAIL_SERVER_PORT')
+ms_username = settings.get('MAIL_SERVER_LOGIN')
+ms_password = settings.get('MAIL_SERVER_PASSWORD')
+ms_recipients = settings.get('ALERT_RECIPIENTS')
+ms_sender = 'Seismic System Automatic Alerting Service <alerts@microquake.org>'
+
+
+class AlertMessage():
+    def __init__(self, host, port, username, password, sender, recipients,
+                 alert_level, alert_topic, message, link_waveform_ui="",
+                 link_3d_ui=""):
+        self.host = host
+        self.port = port
+        self.username = username
+        self.password = password
+        self.sender = sender
+        self.recipients = recipients
+        self.alert_level = alert_level
+        self.alert_topic = alert_topic
+        self.link_waveform_ui = link_waveform_ui
+        self.link_3d_ui = link_3d_ui
+        self.message_core = message
+        self.message = """From: {sender}
+To: Jean-Philippe Mercier <jpmercier01@gmail.com>
+Subject: MICROQUAKE ALERT / SEVERITY LEVEL {alert_level} / {alert_topic}
+
+{message}
+
+{link_waveform_ui}
+{link_3d_ui}
+        """.format(sender=sender, recipients=recipients,
+                   alert_level=alert_level, alert_topic=alert_topic,
+                   message=message, link_waveform_ui=link_waveform_ui,
+                   link_3d_ui=link_3d_ui)
+
+    def send_message(self):
+
+        sender = 'alerts@microquake.org'
+        recipients = 'jpmercier01@gmail.com'
+
+        try:
+            server = smtplib.SMTP_SSL(self.host, self.port)
+            server.ehlo()
+            server.login(self.username, self.password)
+            server.sendmail(self.sender, self.recipients, self.message)
+            server.close()
+            logger.info("Successfully sent email")
+        except Exception as e:
+            logger.error("Error: unable to send email")
+            logger.error(e)
+
+
+# types of alerts
+# 1 - Connector is not working
+# 2 - Number of sensor offline
+# 3 - Large events
+# 4 - Event rates
+# 5 - API is not reachable or down
+
+# host = 'mail.gandi.net'
+# port = 465
+# username = 'alerts@microquake.org'
+# password = 'mfY-pqu-dXD-9MZ'
+
+# message = """From: Seismic Alerts <alerts@microquake.org>
+# To: Jean-Philippe Mercier <jpmercier01@gmail.com>
+# Subject: MICROQUAKE ALERT <ALERT_LEVEL>: <ALERT_TOPIC>
+#
+# <MESSAGE>
+#
+# <LINKS_WAVEFORM_UI>
+# <LINKS_3D_UI>
+# """
+
+api_base_url = settings.get('api_base_url')
+
+if api_base_url[-1] != '/':
+    api_base_url = '/'
+
+# send_message(message)
+
+# Connector check threshold
+
+magnitude_threshold = settings.get('ALERT_EVENT_RATE_THRESHOLD')
+event_rate_level_1 = settings.get('alert_event_rate_level_1')
+event_rate_level_2 = settings.get('alert_event_rate_level_2')
+event_rate_level_3 = settings.get('alert_event_rate_level_3')
+
+
+def seismic_activity_rate(end_time=datetime.now(),
+                          window_size_hours=1,
+                          mean_shift_bandwidth=100):
+    """
+    Find seismic event cluster and calculate the seismic event rate within
+    those cluster. Returns information about the cluster where activity rate
+    exceeds the predefined threshold. The clustering is performed using the
+    MeanShift algorithm. The mean shift algorithm is parameterized by it
+    bandwidth. The bandwidth is basically the size of the kernel
+    :return:
+    """
+    start_time = end_time - timedelta(hours=window_size_hours)
+
+    res = get_catalog(api_base_url, start_time, end_time,
+                      event_type='seismic event', status='accepted')
+
+    if len(res) < event_rate_level_1:
+        logger.info(f'The number of events {len(res)} within the time window '
+                    f'is smaller than the threshold for level 1 '
+                    f'({event_rate_level_1}). Exiting!')
+        return 1
+
+    x = []
+    y = []
+    z = []
+    mag = []
+    ev_time = []
+    for re in res:
+        # if re.magnitude > magnitude_threshold:
+            x.append(re.x)
+            y.append(re.y)
+            z.append(re.z)
+            mag.append(re.magnitude)
+            ev_time.append(re.time_utc)
+
+    if len(x) < event_rate_level_1:
+        logger.info(f'The number of events {len(res)} within the time window '
+                    f'is smaller than the threshold for level 1 '
+                    f'({event_rate_level_1}). Exiting!')
+        return 1
+
+
+    df = pd.DataFrame({'x': x, 'y': y, 'z': z})
+
+    # bandwidth = settings.get('clustering_mean_shift_bandwidth_meter')
+    bandwidth = mean_shift_bandwidth
+
+    logger.info(f'Clustering using the Mean Shift algorithm using a '
+                f'bandwidth of {bandwidth} meter.')
+
+    # estimate_bandwidth(df, quantile=0.2)
+    ms = MeanShift(bandwidth=bandwidth )
+    ms.fit(df)
+    labels = ms.predict(df)
+    centroids = ms.cluster_centers_
+
+    logger.info(f'{len(np.unique(labels))} cluster found')
+
+    df['label'] = labels
+    df['magnitude'] = mag
+    df['ev_time'] = ev_time
+
+    df_label = df.groupby('label')
+
+    clusters = []
+    for i, key in enumerate(df_label.groups.keys()):
+        group = df_label.groups[key]
+        if len(group) < event_rate_level_1:
+            continue
+
+        cluster = {'number_of_events' : len(group),
+                   'activity_rate_hours': len(group) / window_size_hours,
+                   'normalized_number_of_events': 0,
+                   'centroids': centroids[i],
+                   'max_magnitude': np.max(df.loc[group])}
+
+        clusters.append(cluster)
+
+    return clusters, df, centroids
+
+
+def alert_heartbeat(test_mode=False):
+    alert_topic = 'Data Connector'
+
+    alert = AlarmingState()
+
+    session, engine = create_postgres_session()
+    obj = session.query(AlarmingState).filter(
+                        AlarmingState.alert_type=='connector').order_by(desc(
+                        'time')).first()
+
+    last_alert = session.query(AlarmingState).filter(
+        AlarmingState.alert_type == 'connector').filter(
+        AlarmingState.alert_sent).order_by(desc('time')).first()
+
+    current_alert_level = 0
+
+    if obj is not None:
+        current_alert_level = obj.alert_level
+
+    if last_alert is not None:
+        last_alert_time = last_alert.time
+
+    AlarmingState.time = datetime.utcnow().replace(tzinfo=utc)
+
+    alert_connector_1 = settings.get('alert_connector_level_1_min')
+    alert_connector_2 = settings.get('alert_connector_level_2_min')
+
+    heartbeat = requests.get(f'{api_base_url}'
+                             f'inventory/heartbeat/event_connector' )
+    last_hb_time = parse(eval(heartbeat.content.decode())['last_heard'])
+    last_hb_time = last_hb_time.replace(tzinfo=utc)
+    last_hb_delay = datetime.utcnow().replace(tzinfo=utc) - last_hb_time
+    last_hb_delay_minute = last_hb_delay.total_seconds() / 60
+
+    alert_recurrence_time = settings.get('ALERT_CONNECTOR_RECURRENCE_TIME_HOUR')
+
+    if test_mode:
+        alert_level = 'test'
+
+        message_core = """
+This is a test for the connector alert module, this message will be 
+sent periodically to ensure the alerting module is active and working. 
+        
+The health status of the connector was last checked {minute:0.2f} minutes ago
+
+The current alert level is {alert_level}
+        
+""".format(minute=last_hb_delay_minute, alert_level=current_alert_level)
+
+        am = AlertMessage(ms_host, ms_port, ms_username, ms_password,
+                          ms_sender, ms_recipients, alert_level,
+                          alert_topic, message_core)
+
+        am.send_message()
+
+        return
+
+    if last_hb_delay_minute > alert_connector_2:
+
+        alert_level = 2
+
+        send_message=True
+
+        if current_alert_level != 2:
+
+            send_message=True
+
+        elif last_alert_time > alert_recurrence_time:
+
+            send_message=True
+
+    elif last_hb_delay_minute > alert_connector_1:
+
+        alert_level = 1
+
+        if current_alert_level != 1:
+
+            send_message=True
+
+    else:
+        alert_level = 0
+
+    alert.time = datetime.utcnow()
+    alert.alert_type = 'connector'
+    alert.alert_level = alert_level
+
+    session.add(alert)
+    session.commit()
+
+    if send_message:
+        message_core.format(alert_level=alert_level,
+                            time_since_last_heart_beat_hour=last_hb_delay_hour,
+                            time_since_last_heart_beat_minutes=
+                            int(last_hb_delay_minute))
+        am = AlertMessage(ms_host, ms_port, ms_username, ms_password,
+                          ms_sender, ms_recipients, alert_level,
+                          alert_topic, message_core)
+
+        am.send_message()
---
+++ spp/alarming/kmean_test.py
@@ -0,0 +1,117 @@
+from microquake.clients.api_client import get_catalog
+from microquake.core.settings import settings
+from datetime import datetime
+import pandas as pd
+from sklearn.cluster import (KMeans, AffinityPropagation, MeanShift,
+                             estimate_bandwidth)
+from sklearn.mixture import GaussianMixture as GMM
+import matplotlib.pyplot as plt
+from sklearn.metrics import silhouette_score
+import numpy as np
+from loguru import logger
+
+api_base_url = settings.get('api_base_url')
+
+start_time = datetime(2019, 12, 14, 10, 23)
+end_time = datetime(2019, 12, 14, 11, 23)
+
+start_time = datetime(2019, 9, 1)
+end_time = datetime.now()
+
+res = get_catalog(api_base_url, start_time, end_time,
+                  event_type='seismic event', status='accepted')
+
+x = []
+y = []
+z = []
+for re in res:
+    x.append(re.x)
+    y.append(re.y)
+    z.append(re.z)
+
+df = pd.DataFrame({'x': x, 'y': y, 'z': z})
+
+bandwidth = estimate_bandwidth(df, quantile=0.2)
+ms = MeanShift(bandwidth=bandwidth)
+ms.fit(df)
+labels = ms.predict(df)
+centroids = ms.cluster_centers_
+
+logger.info(f'{len(np.unique(labels))} cluster found')
+
+df['label'] = labels
+df['magnitude'] = mag
+df['ev_time'] = ev_time
+
+df_label = df.groupby('label')
+
+
+min_centroid_distance = 150  # minimum distance between centroid in m
+
+df = pd.DataFrame({'x': x, 'y': y, 'z': z})
+
+sil = []
+ks = []
+min_dists = []
+for k in range(2, 20):
+    kmeans = KMeans(n_clusters=k).fit(df)
+    labels = kmeans.predict(df)
+    centroids = kmeans.cluster_centers_
+    dists = []
+    for i, centroid_i in enumerate(centroids):
+        for j, centroid_j in enumerate(centroids[i+1:]):
+            dists.append(np.linalg.norm(centroid_i - centroid_j))
+
+    min_dists.append(np.min(dists))
+    # sil.append(silhouette_score(df, labels=labels, metric='euclidean'))
+    ks.append(k)
+
+for i, k in enumerate(range(2, 20)):
+    if min_dists[i] < min_centroid_distance:
+        break
+
+if k != 0:
+    k -= 1
+
+
+# i = np.argmax(sil)
+# k = ks[i]
+
+kmeans = KMeans(n_clusters=k).fit(df)
+labels = kmeans.predict(df)
+centroids = kmeans.cluster_centers_
+
+plt.clf()
+plt.scatter(x, y, c=labels)
+plt.scatter(centroids[:,0], centroids[:,1], marker='x')
+plt.savefig('gugu.png')
+
+
+
+# gmm = GMM(n_components=4, covariance_type='full', random_state=42).fit(df)
+# labels = gmm.predict(df)
+# # centroids = gmm.cluster_centers_
+
+# plt.clf()
+# plt.scatter(x, y, c=labels)
+# # plt.plot(centroids[:,0], centroids[:,1], 'x')
+# plt.savefig('gugu.png')
+
+#
+#
+# min_distance_centroid = 50  # distance in the unit of measure
+#                             # (the unit of measure is likely to be meter)
+#
+# n_clusters = 20
+# kmeans = KMeans(n_clusters=n_clusters)
+# kmeans.fit(df)
+#
+# ap = AffinityPropagation(preference=-50).fit(df)
+#
+# bandwidth = estimate_bandwidth(df, quantile=0.1)
+# ms = MeanShift(bandwidth=bandwidth)
+# ms.fit(df)
+# labels = ms.predict(df)
+
+
+
