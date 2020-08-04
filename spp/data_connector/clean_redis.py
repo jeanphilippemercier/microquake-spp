63403ecb72035198ebdf8d647248a20b50eb471a
Jean-Philippe <jean-philippem@riotinto.com>
2020-08-04 17:18:26 -0400

    adding a script to clean the redis data base

---
+++ spp/data_connector/clean_redis.py
@@ -0,0 +1,12 @@
+# For some reasons the expired keys are not deleted by the redis database
+# this script removes the expired keys
+
+from spp.db.connectors import connect_redis
+r = connect_redis()
+
+keys = []
+expired_keys = []
+for key in r.scan_iter():
+    print(f'{key} {r.ttl(key)}')
+    if r.ttl(key) == -1:
+        r.delete(key)
