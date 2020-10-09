309e7ee8be66c6801091199da0c13b8f1eb23840
jpmercier <jpmercier01@gmail.com>
2020-10-09 17:19:16 -0400

    adjusting the settings

--- spp/core/helpers/logging.py
+++ spp/core/helpers/logging.py
@@ -3,6 +3,6 @@ import sys
 
 from spp.core.settings import settings
 
-logging_level = settings.get('debug_level')
+logging_level = settings.DEBUG_LEVEL
 logger.add(sys.stderr, level=logging_level)
 logger.remove(0)
